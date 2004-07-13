ecb1d3de178ce712b5ed155da8c9e0d7e9d9a873
chregu <chregu@99f7f646-bdd5-0310-89f9-ddcb10b10bf3>
2004-07-13 09:05:52 +0000

    more descriptive error messages
    git-svn-id: https://svn.liip.ch/repos/public/popoon/trunk@1747 99f7f646-bdd5-0310-89f9-ddcb10b10bf3

--- exceptions/XMLParseError.php
+++ exceptions/XMLParseError.php
@@ -1,10 +1,23 @@
 <?php
 class PopoonXMLParseErrorException extends Exception {
     
-    function __construct($filename) {
+    public function __construct($filename) {
+        set_error_handler(array($this,"errorHandler"));
+        $dom = new DomDocument();
+        $dom->load($filename);
+        restore_error_handler();
         //FIXME: Give more info, what went wrong
         $this->message = "XML Parse Error in $filename";
         parent::__construct();
+    }
+    
+    public function errorHandler($errno, $errstr, $errfile, $errline) 
+    {
+        
+        $errstr = substr($errstr,strpos($errstr,"]:") + 2);
+        
+        $this->userInfo .="$errstr<br />\n";
+        
     }
 }
 ?>
