c83fb33675fbf11cf730a7707e472c3c9879fc4f
chregu <chregu@99f7f646-bdd5-0310-89f9-ddcb10b10bf3>
2004-09-13 08:25:10 +0000

    a component to highlight google search terms, if someone comes from google ;)
    git-svn-id: https://svn.liip.ch/repos/public/popoon/trunk@2355 99f7f646-bdd5-0310-89f9-ddcb10b10bf3

---
+++ components/transformers/searchhighlighter.php
@@ -0,0 +1,67 @@
+<?php
+// +----------------------------------------------------------------------+
+// | popoon                                                               |
+// +----------------------------------------------------------------------+
+// | Copyright (c) 2001,2002,2003,2004 Bitflux GmbH                       |
+// +----------------------------------------------------------------------+
+// | Licensed under the Apache License, Version 2.0 (the "License");      |
+// | you may not use this file except in compliance with the License.     |
+// | You may obtain a copy of the License at                              |
+// | http://www.apache.org/licenses/LICENSE-2.0                           |
+// | Unless required by applicable law or agreed to in writing, software  |
+// | distributed under the License is distributed on an "AS IS" BASIS,    |
+// | WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or      |
+// | implied. See the License for the specific language governing         |
+// | permissions and limitations under the License.                       |
+// +----------------------------------------------------------------------+
+// | Author: Christian Stocker <chregu@bitflux.ch>                        |
+// +----------------------------------------------------------------------+
+//
+// $Id$
+
+include_once("popoon/components/transformer.php");
+
+/**
+* Transforms an XML-Document with the help of libxslt out of domxml
+*
+* @author   Christian Stocker <chregu@bitflux.ch>
+* @version  $Id$
+* @package  popoon
+*/
+class popoon_components_transformers_searchhighlighter extends popoon_components_transformer {
+    
+    public $XmlFormat = "Own";
+    
+    
+    function __construct(&$sitemap) {
+        parent::__construct($sitemap);
+    }
+    
+    function DomStart(&$xml)
+    {   
+        if (isset($_SERVER['HTTP_REFERER'])) {
+            
+            if (strpos($_SERVER['HTTP_REFERER'],"q=")) {
+                $ref = parse_url($_SERVER['HTTP_REFERER']);
+                parse_str($ref['query'],$para);
+                $query =  $para['q'];
+                if ($query) {
+                    parent::DomStart($xml);
+                    popoon_sitemap::var2XMLString($xml);
+                    $strings = explode(" ", $query);
+                    $search = array();
+                    foreach ($strings as $st) {
+                        $search[] = '#(>[^<]*)('.$st .')#i';
+                    }
+                    $repl = '$1<span class="searchHighlight">$2</span>';
+                    $xml = preg_replace($search,$repl,$xml);
+                }
+            }
+        }
+        
+        
+    }
+}
+
+
+?>
