ca9c4425b4d3cc5e728c1ad0bee5dec46dc05644
chregu <chregu@99f7f646-bdd5-0310-89f9-ddcb10b10bf3>
2004-10-07 12:42:00 +0000

    don't leak username and password to the outside
    git-svn-id: https://svn.liip.ch/repos/public/popoon/trunk@2741 99f7f646-bdd5-0310-89f9-ddcb10b10bf3

--- exceptions/DB.php
+++ exceptions/DB.php
@@ -3,7 +3,10 @@ class PopoonDBException extends Exception {
     
     function __construct($err) {
         $this->message = $err->getMessage();
-        $this->userInfo =   $err->getUserInfo();
+        //don't leak username:password to the outside
+        
+        $this->userInfo =   preg_replace("#//([^:]*):([^\@^:]*)\@#","//*******:********@",$err->getUserInfo());
+         
         parent::__construct();
     }
 }
