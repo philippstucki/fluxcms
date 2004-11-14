9b817c7bdb70edfbe8b04dc1aa73dab6fa4f0bc5
chregu <chregu@99f7f646-bdd5-0310-89f9-ddcb10b10bf3>
2004-11-14 21:28:49 +0000

    fix an issue with & and html_entity_decode
    git-svn-id: https://svn.liip.ch/repos/public/popoon/trunk@2986 99f7f646-bdd5-0310-89f9-ddcb10b10bf3

--- classes/externalinput.php
+++ classes/externalinput.php
@@ -7,13 +7,17 @@ class popoon_classes_externalinput {
     // use it whereever you get external input    
 
     static function basicClean($string) {
+        
         if (get_magic_quotes_gpc()) {
             $string = stripslashes($string);
         }
-        $string = html_entity_decode($string);
+        $string = html_entity_decode($string, ENT_COMPAT, "UTF-8");
         $string = preg_replace('#</*(script|embed|object)[^>]*>#i',"",$string);
         $string = preg_replace('#(<[^>]+)\son[^>]*>#iU',"$1>",$string);
         $string = preg_replace('#([a-z]*)\s*=\s*([\'\"]*)\s*j\s*a\s*v\s*a\s*s\s*c\s*r\s*i\s*p\s*t\s*:#i','$1=$2nojavascript...',$string);
+        //convert & back to &amp; 
+        $string = str_replace("&","&amp;",$string);
+        
         return $string;
     }
 }
