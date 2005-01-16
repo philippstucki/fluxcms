b33cbd9e80286d22e4eba83ced082f190901d0cc
chregu <chregu@99f7f646-bdd5-0310-89f9-ddcb10b10bf3>
2005-01-16 16:22:56 +0000

    remove also xmlns attributes
    git-svn-id: https://svn.liip.ch/repos/public/popoon/trunk@3433 99f7f646-bdd5-0310-89f9-ddcb10b10bf3

--- classes/externalinput.php
+++ classes/externalinput.php
@@ -14,9 +14,13 @@ class popoon_classes_externalinput {
         // fix &entitiy\n;
         $string = preg_replace('#(&\#*\w+)[\s\r\n]+;#U',"$1;",$string);
         $string = html_entity_decode($string, ENT_COMPAT, "UTF-8");
+        //remove really unwanted tags
         $string = preg_replace('#</*(script|embed|object|iframe|frame|frameset|ilayer|layer|bgsound|title|base)[^>]*>#i',"",$string);
+        //remove namespaced elements (we do not need them...)
         $string = preg_replace('#</*\w+:\w[^>]*>#i',"",$string);
-        $string = preg_replace('#(<[^>]+[\s\r\n\"\'])on[^>]*>#iU',"$1>",$string);
+        // remove any attribute starting with "on" or xmlns
+        $string = preg_replace('#(<[^>]+[\s\r\n\"\'])(on|xmlns)[^>]*>#iU',"$1>",$string);
+        // remove javascript: and vbscript: protocol
         $string = preg_replace('#([a-z]*)[\s\r\n]*=[\s\r\n]*([\'\"]*)[\s\r\n]*javascript[\s\r\n]*:#iU','$1=$2nojavascript...',$string);
         $string = preg_replace('#([a-z]*)[\s\r\n]*=[\s\r\n]*([\'\"]*)[\s\r\n]*vbscript[\s\r\n]*:#iU','$1=$2novbscript...',$string);
         //<span style="width: expression(alert('Ping!'));"></span> 
