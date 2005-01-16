9f8075545ea01f7bf3929256fb1cb566cb7da8c4
chregu <chregu@99f7f646-bdd5-0310-89f9-ddcb10b10bf3>
2005-01-16 16:13:36 +0000

    remove namespaced elements
    git-svn-id: https://svn.liip.ch/repos/public/popoon/trunk@3432 99f7f646-bdd5-0310-89f9-ddcb10b10bf3

--- classes/externalinput.php
+++ classes/externalinput.php
@@ -12,9 +12,10 @@ class popoon_classes_externalinput {
         }
         $string = str_replace(array("&amp;","&lt;","&gt;"),array("&amp;amp;","&amp;lt;","&amp;gt;",),$string);
         // fix &entitiy\n;
-        $string = preg_replace('#(&\#*\w+)[\s\r\n]+;#',"$1;",$string);
+        $string = preg_replace('#(&\#*\w+)[\s\r\n]+;#U',"$1;",$string);
         $string = html_entity_decode($string, ENT_COMPAT, "UTF-8");
-        $string = preg_replace('#</*(script|embed|object|iframe)[^>]*>#i',"",$string);
+        $string = preg_replace('#</*(script|embed|object|iframe|frame|frameset|ilayer|layer|bgsound|title|base)[^>]*>#i',"",$string);
+        $string = preg_replace('#</*\w+:\w[^>]*>#i',"",$string);
         $string = preg_replace('#(<[^>]+[\s\r\n\"\'])on[^>]*>#iU',"$1>",$string);
         $string = preg_replace('#([a-z]*)[\s\r\n]*=[\s\r\n]*([\'\"]*)[\s\r\n]*javascript[\s\r\n]*:#iU','$1=$2nojavascript...',$string);
         $string = preg_replace('#([a-z]*)[\s\r\n]*=[\s\r\n]*([\'\"]*)[\s\r\n]*vbscript[\s\r\n]*:#iU','$1=$2novbscript...',$string);
