defa8ef54e6a24876edf0f690a02464ff37711a3
chregu <chregu@99f7f646-bdd5-0310-89f9-ddcb10b10bf3>
2005-01-17 19:51:32 +0000

    more updates
    git-svn-id: https://svn.liip.ch/repos/public/popoon/trunk@3443 99f7f646-bdd5-0310-89f9-ddcb10b10bf3

--- classes/externalinput.php
+++ classes/externalinput.php
@@ -24,14 +24,14 @@ class popoon_classes_externalinput {
         //<span style="width: expression(alert('Ping!'));"></span> 
         // only works in ie...
         $string = preg_replace('#(<[^>]+)style[\s\r\n]*=[\s\r\n]*([\`\'\"]*).*expression[\s\r\n]*\([^>]*>#iU',"$1>",$string);
-        $string = preg_replace('#(<[^>]+)style[\s\r\n]*=[\s\r\n]*([\`\'\"]*).*j[\s\n\r]*a[\s\n\r]*v[\s\n\r]*a[\s\n\r]*s[\s\n\r]*c[\s\n\r]*r[\s\n\r]*i[\s\n\r]*p[\s\n\r]*t[\s\n\r]*:[^>]*>#iU',"$1>",$string);
+        $string = preg_replace('#(<[^>]+)style[\s\r\n]*=[\s\r\n]*([\`\'\"]*).*s[\s\n\r]*c[\s\n\r]*r[\s\n\r]*i[\s\n\r]*p[\s\n\r]*t[\s\n\r]*:*[^>]*>#iU',"$1>",$string);
         //remove namespaced elements (we do not need them...)
         $string = preg_replace('#</*\w+:\w[^>]*>#i',"",$string);
         //remove really unwanted tags
         
         do {
             $oldstring = $string;
-            $string = preg_replace('#</*(script|embed|object|iframe|frame|frameset|ilayer|layer|bgsound|title|base)[^>]*>#i',"",$string);
+            $string = preg_replace('#</*(style|script|embed|object|iframe|frame|frameset|ilayer|layer|bgsound|title|base)[^>]*>#i',"",$string);
         } while ($oldstring != $string);
         
         return $string;
