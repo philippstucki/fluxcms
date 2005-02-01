b7cbcf40b1e231a95ec8a5765f2b7b5ccc205949
chregu <chregu@99f7f646-bdd5-0310-89f9-ddcb10b10bf3>
2005-02-01 08:15:44 +0000

    add removeDefaultPrefix option
    git-svn-id: https://svn.liip.ch/repos/public/popoon/trunk@3558 99f7f646-bdd5-0310-89f9-ddcb10b10bf3

--- components/serializers/xml.php
+++ components/serializers/xml.php
@@ -47,6 +47,9 @@ class popoon_components_serializers_xml extends popoon_components_serializer {
             $this->sitemap->hasFinalDom = true;
             $xml = str_replace("HTML","html",$xml->saveXML());
         }
+        if ($this->getParameterDefault("removeDefaultPrefix")) {
+                $xml = preg_replace("#(</?)default:#","$1",$xml);
+        }
         // Mozilla does not display the XML neatly, if there's a xhtml namespace in it, so we spoof it here (mainly used for XML=1 purposes)
         if ($this->getParameterDefault("trickMozillaDisplay")) {
             print  str_replace("http://www.w3.org/1999/xhtml","http://www.w3.org/1999/xhtml#trickMozillaDisplay",$xml);
