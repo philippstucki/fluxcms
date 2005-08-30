e5d8bf47529fbd321b5114d15be37228f4d798b1
benjamin <benjamin@99f7f646-bdd5-0310-89f9-ddcb10b10bf3>
2005-08-30 14:23:31 +0000

    escape error messages
    git-svn-id: https://svn.liip.ch/repos/public/popoon/trunk@5351 99f7f646-bdd5-0310-89f9-ddcb10b10bf3

--- exceptions/DB.php
+++ exceptions/DB.php
@@ -2,10 +2,10 @@
 class PopoonDBException extends Exception {
     
     function __construct($err) {
-        $this->message = $err->getMessage();
+        $this->message =htmlspecialchars( $err->getMessage());
         //don't leak username:password to the outside
         
-        $this->userInfo =   preg_replace("#//([^:]*):([^\@^:]*)\@#","//*******:********@",$err->getUserInfo());
+        $this->userInfo =  htmlspecialchars( preg_replace("#//([^:]*):([^\@^:]*)\@#","//*******:********@",$err->getUserInfo()));
         $this->code = $err->getCode();
         parent::__construct();
     }
