ec1cc9306f065e83e4386b914d199848d9457981
bitflux <bitflux@99f7f646-bdd5-0310-89f9-ddcb10b10bf3>
2005-09-08 09:16:46 +0000

    removed htmlspecialchars, we do that in the error generator now
    git-svn-id: https://svn.liip.ch/repos/public/popoon/trunk@5500 99f7f646-bdd5-0310-89f9-ddcb10b10bf3

--- exceptions/DB.php
+++ exceptions/DB.php
@@ -2,10 +2,10 @@
 class PopoonDBException extends Exception {
     
     function __construct($err) {
-        $this->message =htmlspecialchars( $err->getMessage());
+        $this->message = $err->getMessage();
         //don't leak username:password to the outside
         
-        $this->userInfo =  htmlspecialchars( preg_replace("#//([^:]*):([^\@^:]*)\@#","//*******:********@",$err->getUserInfo()));
+        $this->userInfo =  preg_replace("#//([^:]*):([^\@^:]*)\@#","//*******:********@",$err->getUserInfo());
         $this->code = $err->getCode();
         parent::__construct();
     }
