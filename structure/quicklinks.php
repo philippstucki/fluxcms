ee90a362e9f25647838af0c39506a988b5ab61e5
chregu <chregu@99f7f646-bdd5-0310-89f9-ddcb10b10bf3>
2006-01-06 06:33:46 +0000

    blog links not needed anymore in quicklinks
    git-svn-id: https://svn.liip.ch/repos/public/fluxcms_demo/trunk@6248 99f7f646-bdd5-0310-89f9-ddcb10b10bf3

--- structure/quicklinks.php
+++ structure/quicklinks.php
@@ -2,11 +2,11 @@
 
 $quicklinks = array (
 'admin/siteoptions/' => $i18n->translate('Site-Options'),
-'blogposts' => "Posts",
-'blogcategories' => $i18n->translate("Post Categories"),
-'blogcomments' => $i18n->translate("Post Comments"),
-'bloglinks' => "Links",
-'bloglinkscategories' => $i18n->translate("Link Categories"),
+//'blogposts' => "Posts",
+//'blogcategories' => $i18n->translate("Post Categories"),
+//'blogcomments' => $i18n->translate("Post Comments"),
+//'bloglinks' => "Links",
+//'bloglinkscategories' => $i18n->translate("Link Categories"),
 
 
 'users' => 'Users',
