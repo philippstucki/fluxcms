6901895dcbd058dd6cdb0a94aec68f59beec8921
chregu <chregu@99f7f646-bdd5-0310-89f9-ddcb10b10bf3>
2006-01-07 17:05:44 +0000

    update
    git-svn-id: https://svn.liip.ch/repos/public/fluxcms/trunk@6278 99f7f646-bdd5-0310-89f9-ddcb10b10bf3

--- inc/bx/tools/generate/plugin.php
+++ inc/bx/tools/generate/plugin.php
@@ -5,10 +5,13 @@ bx_init::start('conf/config.xml', "");
 define('PHP_CLASSPATH', getcwd(). "/install/phing/classes/" . PATH_SEPARATOR . getcwd()."/inc/". PATH_SEPARATOR . get_include_path());
 ini_set('include_path', PHP_CLASSPATH);
 require_once 'phing/Phing.php';
+require_once 'inc/bx/tools/generate/db2forms.php';
+
 
 /* Setup Phing environment */
 Phing::startup();
 
+ 
 
 $generateDir =  getcwd()."/inc/bx/tools/generate/";
 Phing::setProperty("BxRootDir",getcwd()."/");
