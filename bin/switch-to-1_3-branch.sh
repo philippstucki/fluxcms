c3ce477bf4fd29ab61b868ab30f9fa4f83c791b1
chregu <chregu@99f7f646-bdd5-0310-89f9-ddcb10b10bf3>
2006-01-10 07:10:54 +0000

    added popoon switcher more generalized
    git-svn-id: https://svn.liip.ch/repos/public/fluxcms_demo/trunk@6317 99f7f646-bdd5-0310-89f9-ddcb10b10bf3

--- bin/switch-to-1_3-branch.sh
+++ bin/switch-to-1_3-branch.sh
@@ -1,46 +1,4 @@
 BRANCH="branches/1_3"
-DIRS="webinc inc admin install"
-STARTDIR=`pwd`
+POPOON_BRANCH="branches/1_1"
 
-echo -e "\nUsually this script works without problems, but nevertheles\nPLEASE DO BACKUP BEFORE.\nPress <Enter> to continue or <Ctrl>+<C> to abort."
-read
-
-svn propget svn:externals . > .externals.old
-echo -n "" > .externals
-
-for i in $DIRS
-do
-echo $i
-cd $i
-svn switch https://svn.bitflux.ch/repos/public/fluxcms/$BRANCH/$i
-cd $STARTDIR
-echo "$i https://svn.bitflux.ch/repos/public/fluxcms/$BRANCH/$i" >> .externals
-done
-#echo "" >> .externals
-
-#themes
-
-cd themes
-DIRS="standard"
-STARTDIR2=`pwd`
-
-svn propget svn:externals . > .externals.old
-echo -n "" > .externals
-
-for i in $DIRS
-do
-echo $i
-cd $i
-svn switch https://svn.bitflux.ch/repos/public/fluxcms/$BRANCH/themes/$i
-cd $STARTDIR2
-echo "$i https://svn.bitflux.ch/repos/public/fluxcms/$BRANCH/themes/$i" >> .externals
-done
-#echo "" >> .externals
-#comment out the followin, if you're not using fluxcms_demo, but your own 
-
-svn propset svn:externals . -F .externals 
-svn switch https://svn.bitflux.ch/repos/public/fluxcms_demo/$BRANCH/themes/
-
-cd $STARTDIR
-svn propset svn:externals . -F .externals 
-svn switch https://svn.bitflux.ch/repos/public/fluxcms_demo/$BRANCH/
+. bin/branch-switcher.sh
