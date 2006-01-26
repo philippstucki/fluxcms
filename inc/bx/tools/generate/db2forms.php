7e2417db9e02115b74cf5efa3b6d48346feeacbc
chregu <chregu@99f7f646-bdd5-0310-89f9-ddcb10b10bf3>
2006-01-26 17:35:30 +0000

    adjust to new MDB2_Schema
    git-svn-id: https://svn.liip.ch/repos/public/fluxcms/trunk@6422 99f7f646-bdd5-0310-89f9-ddcb10b10bf3

--- inc/bx/tools/generate/db2forms.php
+++ inc/bx/tools/generate/db2forms.php
@@ -1,26 +1,42 @@
 <?php
+if (strpos($argv[0], basename(__FILE__)) > 0 && $argv[1]) {
 
+require_once("inc/bx/init.php");
+bx_init::start('conf/config.xml', "");
+
+db2forms($argv[1]);
+
+
+}
 function db2forms($table = "") {
     if (!$table) {
         return ;
     }
     $dsn = $GLOBALS['POOL']->config->dsn;
     require_once 'MDB2.php';
-    MDB2::loadFile('Tools/Manager');
-    $manager = new MDB2_Tools_Manager();
+    MDB2::loadFile('Schema');
+    $manager = new MDB2_Schema();
+    
     $err = $manager->connect($dsn, array('debug' => false, 'log_line_break' => '\n'));
     if (MDB2::isError($err)) {
-        $error = $err->getMessage();
-        
+        print $err->getMessage();
+        print "\n";
     } else {
         $GLOBALS['db2forms_output'] = "";
-        $dump_what = MDB2_MANAGER_DUMP_STRUCTURE;
+        $dump_what = MDB2_SCHEMA_DUMP_STRUCTURE;
         $dump_config = array(
         'output' => 'db2forms_output'
         );
-         $manager->dumpDatabase($dump_config, $dump_what);
+        $err = $manager->dumpDatabase($dump_config, $dump_what);
+        if (MDB2::isError($err)) {
+            //bx_helpers_debug::webdump($err);
+            print  $err->getMessage();;
+            print "\n";
+            print  $err->getUserInfo();;
+            print "\n";
+        }
     }
-    
+    if ($GLOBALS['db2forms_output']) {
     $xsl = new xsltprocessor();
     
     $xsl->importStylesheet(domdocument::load(BX_PROJECT_DIR."inc/bx/tools/generate/schema2forms.xsl"));
@@ -32,6 +48,10 @@ function db2forms($table = "") {
     $dom->formatOutput = 1;
     
     $dom->save(BX_PROJECT_DIR."/dbforms2/$table.xml");
+    print "done in ".BX_PROJECT_DIR."/dbforms2/$table.xml\n";
+    } else {
+        print "No Output produced by MDB2\n";
+    }
     
 }
 
