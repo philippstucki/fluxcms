a27bc72ae954cf8a447507f45d129db551d34b2b
philipp <philipp@99f7f646-bdd5-0310-89f9-ddcb10b10bf3>
2006-02-08 16:24:39 +0000

    added mouse over/out events
    git-svn-id: https://svn.liip.ch/repos/public/fluxcms/trunk@6463 99f7f646-bdd5-0310-89f9-ddcb10b10bf3

--- webinc/js/bx/scroller.js
+++ webinc/js/bx/scroller.js
@@ -10,6 +10,10 @@ var BX_SCROLLER_SCROLLING = 1;
 var BX_SCROLLER_STARTING = 4;
 var BX_SCROLLER_STOPPING = 8;
 
+var BX_SCROLLER_EV_MOUSEOVER = 1;
+var BX_SCROLLER_EV_MOUSEDOWN = 1;
+
+
 function bx_scroller() {
     
     // scroll interval in ms
@@ -30,6 +34,8 @@ function bx_scroller() {
     this.buttonUpNode = null;
     this.buttonDownNode = null;
     
+    this.overOut = true;
+    
     this.init = function(scrollNode) {
         this.scrollNode = scrollNode;
     }
@@ -38,21 +44,35 @@ function bx_scroller() {
         this.buttonUpNode = buttonUpNode;
         this.buttonDownNode = buttonDownNode;
 
-        if(this.buttonUpNode) {
+        if(this.buttonUpNode && this.overOut) {
+            // button up, mouse over
+            var wev_buttonUpOnMouseOver = new bx_helpers_contextfixer(this.e_buttonUpOnMouseOver, this);
+            bx_helpers.addEventListener(this.buttonUpNode, 'mouseover', wev_buttonUpOnMouseOver.execute);
+            // button up, mouse out
+            var wev_buttonUpOnMouseOut = new bx_helpers_contextfixer(this.e_buttonUpOnMouseOut, this);
+            bx_helpers.addEventListener(this.buttonUpNode, 'mouseout', wev_buttonUpOnMouseOut.execute);
+
+        } else if(this.buttonUpNode) {
             // button up, mouse down
             var wev_buttonUpOnMouseDown = new bx_helpers_contextfixer(this.e_buttonUpOnMouseDown, this);
             bx_helpers.addEventListener(this.buttonUpNode, 'mousedown', wev_buttonUpOnMouseDown.execute);
-            
             // button up, mouse up
             var wev_buttonUpOnMouseUp = new bx_helpers_contextfixer(this.e_buttonUpOnMouseUp, this);
             bx_helpers.addEventListener(this.buttonUpNode, 'mouseup', wev_buttonUpOnMouseUp.execute);
         }
         
-        if(this.buttonDownNode) {
+        if(this.buttonDownNode && this.overOut) {
+            // button down, mouse over
+            var wev_buttonDownOnMouseOver = new bx_helpers_contextfixer(this.e_buttonDownOnMouseOver, this);
+            bx_helpers.addEventListener(this.buttonDownNode, 'mouseover', wev_buttonDownOnMouseOver.execute);
+            // button down, mouse out
+            var wev_buttonDownOnMouseOut = new bx_helpers_contextfixer(this.e_buttonDownOnMouseOut, this);
+            bx_helpers.addEventListener(this.buttonDownNode, 'mouseout', wev_buttonDownOnMouseOut.execute);
+
+        } else if(this.buttonDownNode) {
             // button down, mouse down
             var wev_buttonDownOnMouseDown = new bx_helpers_contextfixer(this.e_buttonDownOnMouseDown, this);
             bx_helpers.addEventListener(this.buttonDownNode, 'mousedown', wev_buttonDownOnMouseDown.execute);
-    
             // button down, mouse up
             var wev_buttonDownOnMouseUp = new bx_helpers_contextfixer(this.e_buttonDownOnMouseUp, this);
             bx_helpers.addEventListener(this.buttonDownNode, 'mouseup', wev_buttonDownOnMouseUp.execute);
@@ -63,21 +83,33 @@ function bx_scroller() {
         this.buttonLeftNode = buttonLeftNode;
         this.buttonRightNode = buttonRightNode;
         
-        if(this.buttonLeftNode) {
+        if(this.buttonLeftNode && this.overOut) {
+            // button left, mouse over
+            var wev_buttonLeftOnMouseOver = new bx_helpers_contextfixer(this.e_buttonLeftOnMouseOver, this);
+            bx_helpers.addEventListener(this.buttonLeftNode, 'mouseover', wev_buttonLeftOnMouseOver.execute);
+            // button left, mouse out
+            var wev_buttonLeftOnMouseOut = new bx_helpers_contextfixer(this.e_buttonLeftOnMouseOut, this);
+            bx_helpers.addEventListener(this.buttonLeftNode, 'mouseout', wev_buttonLeftOnMouseOut.execute);
+        } else {
             // button left, mouse down
             var wev_buttonLeftOnMouseDown = new bx_helpers_contextfixer(this.e_buttonLeftOnMouseDown, this);
             bx_helpers.addEventListener(this.buttonLeftNode, 'mousedown', wev_buttonLeftOnMouseDown.execute);
-    
             // button left, mouse up
             var wev_buttonLeftOnMouseUp = new bx_helpers_contextfixer(this.e_buttonLeftOnMouseUp, this);
             bx_helpers.addEventListener(this.buttonLeftNode, 'mouseup', wev_buttonLeftOnMouseUp.execute);
         }
         
-        if(this.buttonRightNode) {
+        if(this.buttonRightNode && this.overOut) {
+            // button right, mouse over
+            var wev_buttonRightOnMouseOver = new bx_helpers_contextfixer(this.e_buttonRightOnMouseOver, this);
+            bx_helpers.addEventListener(this.buttonRightNode, 'mouseover', wev_buttonRightOnMouseOver.execute);
+            // button right, mouse out
+            var wev_buttonRightOnMouseOut = new bx_helpers_contextfixer(this.e_buttonRightOnMouseOut, this);
+            bx_helpers.addEventListener(this.buttonRightNode, 'mouseout', wev_buttonRightOnMouseOut.execute);
+        } else {
             // button right, mouse down
             var wev_buttonRightOnMouseDown = new bx_helpers_contextfixer(this.e_buttonRightOnMouseDown, this);
             bx_helpers.addEventListener(this.buttonRightNode, 'mousedown', wev_buttonRightOnMouseDown.execute);
-    
             // button right, mouse up
             var wev_buttonRightOnMouseUp = new bx_helpers_contextfixer(this.e_buttonRightOnMouseUp, this);
             bx_helpers.addEventListener(this.buttonRightNode, 'mouseup', wev_buttonRightOnMouseUp.execute);
@@ -106,15 +138,24 @@ function bx_scroller() {
         this.clearInterval();
         
         this.direction = direction;
-        this.scrolling = BX_SCROLLER_SCROLLING | BX_SCROLLER_STARTING;
-        this.currentPpi = 0;
+        this.scrolling = BX_SCROLLER_SCROLLING;
+        this.currentPpi = this.ppi
+        
+        if(this.startAcceleration != 0) {
+            this.scrolling |=  BX_SCROLLER_STARTING;
+            this.currentPpi = 0;
+        }
 
         // start the interval
         this.startInterval();
     }
     
     this.stopScrolling = function() {
-        this.scrolling = BX_SCROLLER_SCROLLING | BX_SCROLLER_STOPPING;
+        if(this.stopAcceleration != 0) {
+            this.scrolling = BX_SCROLLER_SCROLLING | BX_SCROLLER_STOPPING;
+        } else {
+            this.scrolling = BX_SCROLLER_STOPPED;
+        }
     }
     
     this.startInterval = function() {
@@ -175,6 +216,12 @@ function bx_scroller() {
     this.e_buttonUpOnMouseUp = function() {
         this.stopScrolling();
     }
+    this.e_buttonUpOnMouseOver = function() {
+        this.startScrolling(BX_SCROLLER_SCROLL_UP);
+    }
+    this.e_buttonUpOnMouseOut = function() {
+        this.stopScrolling();
+    }
     
     // button down
     this.e_buttonDownOnMouseDown = function() {
@@ -183,6 +230,13 @@ function bx_scroller() {
     this.e_buttonDownOnMouseUp = function() {
         this.stopScrolling();
     }
+    this.e_buttonDownOnMouseOver = function() {
+        this.startScrolling(BX_SCROLLER_SCROLL_DOWN);
+    }
+    this.e_buttonDownOnMouseOut = function() {
+        this.stopScrolling();
+    }
+    
     
     // button left
     this.e_buttonLeftOnMouseDown = function() {
@@ -191,12 +245,24 @@ function bx_scroller() {
     this.e_buttonLeftOnMouseUp = function() {
         this.stopScrolling();
     }
+    this.e_buttonLeftOnMouseOver = function() {
+        this.startScrolling(BX_SCROLLER_SCROLL_LEFT);
+    }
+    this.e_buttonLeftOnMouseOut = function() {
+        this.stopScrolling();
+    }
     
     // button right
     this.e_buttonRightOnMouseDown = function() {
         this.startScrolling(BX_SCROLLER_SCROLL_RIGHT);
     }
     this.e_buttonRightOnMouseUp = function() {
+        this.stopScrolling();
+    }
+    this.e_buttonRightOnMouseOver = function() {
+        this.startScrolling(BX_SCROLLER_SCROLL_RIGHT);
+    }
+    this.e_buttonRightOnMouseOut = function() {
         this.stopScrolling();
     }
     
