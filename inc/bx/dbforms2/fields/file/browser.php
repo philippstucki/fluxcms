c53bb5fc30de3802b38c1705cbedd1f474152bf6
philipp <philipp@99f7f646-bdd5-0310-89f9-ddcb10b10bf3>
2006-02-13 19:00:55 +0000

    - preview for image browser
    git-svn-id: https://svn.liip.ch/repos/public/fluxcms/trunk@6492 99f7f646-bdd5-0310-89f9-ddcb10b10bf3

--- inc/bx/dbforms2/fields/file/browser.php
+++ inc/bx/dbforms2/fields/file/browser.php
@@ -15,6 +15,17 @@ class bx_dbforms2_fields_file_browser extends bx_dbforms2_field {
         parent::__construct($name);
         $this->type = 'file_browser';
         $this->XMLName = 'input';
+        $this->attributes['isImage'] = FALSE;
+    }
+
+    protected function getXMLAttributes() {
+        return array('isImage' => $this->attributes['isImage'] === TRUE ? '1' : '0');
+    }
+    
+    public function getConfigAttributes() {
+        $ret =  parent::getConfigAttributes();
+        $ret['isImage'] = 'boolean';
+        return $ret;
     }
     
     
