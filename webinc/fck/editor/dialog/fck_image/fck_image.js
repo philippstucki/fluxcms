5aae6955a5bab16eaa1e0b265dc761fea8c90bb8
chregu <chregu@99f7f646-bdd5-0310-89f9-ddcb10b10bf3>
2006-02-15 13:26:43 +0000

    some integration of fck into bxe (not finished)
    git-svn-id: https://svn.liip.ch/repos/public/fluxcms/trunk@6511 99f7f646-bdd5-0310-89f9-ddcb10b10bf3

--- webinc/fck/editor/dialog/fck_image/fck_image.js
+++ webinc/fck/editor/dialog/fck_image/fck_image.js
@@ -48,6 +48,7 @@ function OnDialogTabChange( tabCode )
 }
 
 // Get the selected image (if available).
+if (FCK.Selection) {
 var oImage = FCK.Selection.GetSelectedElement() ;
 
 if ( oImage && oImage.tagName != 'IMG' && !( oImage.tagName == 'INPUT' && oImage.type == 'image' ) )
@@ -55,7 +56,9 @@ if ( oImage && oImage.tagName != 'IMG' && !( oImage.tagName == 'INPUT' && oImage
 
 // Get the active link.
 var oLink = FCK.Selection.MoveToAncestorNode( 'A' ) ;
-
+} else {
+	oImage = null;
+}
 var oImageOriginal ;
 
 function UpdateOriginal( resetSize )
@@ -226,6 +229,7 @@ function Ok()
 function UpdateImage( e, skipId )
 {
 	e.src = GetE('txtUrl').value ;
+	SetAttribute( e, "src", GetE('txtUrl').value );
 	SetAttribute( e, "alt"   , GetE('txtAlt').value ) ;
 	SetAttribute( e, "width" , GetE('txtWidth').value ) ;
 	SetAttribute( e, "height", GetE('txtHeight').value ) ;
