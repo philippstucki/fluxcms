5be638ee5ac1cb747cbc023dbf7d30be2c38eea7
chregu <chregu@99f7f646-bdd5-0310-89f9-ddcb10b10bf3>
2006-03-02 15:53:48 +0000

    add dbforms2 example
    git-svn-id: https://svn.liip.ch/repos/public/fluxcms_demo/trunk@6576 99f7f646-bdd5-0310-89f9-ddcb10b10bf3

--- structure/quicklinks.php
+++ structure/quicklinks.php
@@ -2,12 +2,7 @@
 
 $quicklinks = array (
 'admin/siteoptions/' => $i18n->translate('Site-Options'),
-//'blogposts' => "Posts",
-//'blogcategories' => $i18n->translate("Post Categories"),
-//'blogcomments' => $i18n->translate("Post Comments"),
-//'bloglinks' => "Links",
-//'bloglinkscategories' => $i18n->translate("Link Categories"),
-
+// 'admin/dbforms2/blogposts/' => 'Blogposts',
 
 'users' => 'Users',
 //'options' => 'options (dbform)',
