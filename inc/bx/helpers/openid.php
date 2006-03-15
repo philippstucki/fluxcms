ed864f1c255ca775785ce22e7debb7d18fc8a25f
chregu <chregu@99f7f646-bdd5-0310-89f9-ddcb10b10bf3>
2006-03-15 14:32:02 +0000

    commit openid changes
    git-svn-id: https://svn.liip.ch/repos/public/fluxcms/trunk@6599 99f7f646-bdd5-0310-89f9-ddcb10b10bf3

--- inc/bx/helpers/openid.php
+++ inc/bx/helpers/openid.php
@@ -4,44 +4,58 @@ class bx_helpers_openid {
     
     static function getServer() {
         if (!isset(self::$server)) {
-            self::$server = new Auth_OpenID_Server(self::getServerURL(), self::getOpenIDStore());
+            $store =  self::getOpenIDStore();
+            $serverURL = self::getServerURL();
+            self::$server = new Auth_OpenID_Server($serverURL, $store);
         }
         return self::$server;
     }
     
     static function getServerURL() {
-        $path = $_SERVER['SCRIPT_NAME'];
-        $host = $_SERVER['HTTP_HOST'];
-        $port = $_SERVER['SERVER_PORT'];
-        $s = $_SERVER['HTTPS'] ? 's' : '';
-        if (($s && $port == "443") || (!$s && $port == "80")) {
-            $p = '';
-        } else {
-            $p = ':' . $port;
-        }
+        return BX_WEBROOT. "admin/webinc/openid/";
         
-        return "http$s://$host$p$path";
     }
     
     static function getOpenIDStore() {    
+        $a = new Auth_OpenID_FileStore("/tmp");
         return new Auth_OpenID_FileStore("/tmp");
     }
-
+    
+    
+    static function setRequestInfo($info=null) {
+        if (!isset($info)) {
+            unset($_SESSION['openid_request']);
+        } else {
+            $_SESSION['openid_request'] = serialize($info);
+        }
+    }
+    
+    static function getRequestInfo() {
+        return isset($_SESSION['openid_request'])
+        ? unserialize($_SESSION['openid_request'])
+        : false;
+    }
+    
     
 }
 
+
+
 /**
- * Return whether the trust root is currently trusted
- */
-function isTrusted($identity_url, $trust_root)
-{
-    // from config.php
+* Return whether the trust root is currently trusted
+*/
+function bx_openIdIsTrusted($identity_url, $trust_root) {
     if ($identity_url != BX_WEBROOT) {
         return false;
     }
-
-    $sites = array();// getSessionSites();
-    //FIXME, get from DB authorized trust_roots
-    return true;
-    return isset($sites[$trust_root]) && $sites[$trust_root];
+    
+    $query = "select uri from ".$GLOBALS['POOL']->config->getTablePrefix()."openid_uri where uri = ".$GLOBALS['POOL']->db->quote($trust_root)." limit 1";
+    $res = $GLOBALS['POOL']->db->query($query);
+    if ($res->numRows() > 0) {
+        return true;
+    }
+    // from config.php
+    
+    return false;
+    
 }
