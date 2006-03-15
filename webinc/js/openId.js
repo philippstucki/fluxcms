d5a334abb2a5259b6a6de19713ad7cc2b25acfa5
benjamin <benjamin@99f7f646-bdd5-0310-89f9-ddcb10b10bf3>
2006-03-15 10:48:28 +0000

    openid stuff
    git-svn-id: https://svn.liip.ch/repos/public/fluxcms/trunk@6590 99f7f646-bdd5-0310-89f9-ddcb10b10bf3

---
+++ webinc/js/openId.js
@@ -0,0 +1,27 @@
+function openId(request){
+  loginWindow = window.open(request.responseText);
+}
+
+function openIdSubmit() {
+    var uri = document.getElementById('baseUri').value;
+    new ajax (liveSearchRoot + 'inc/bx/php/openid/start_auth.php', {
+	postBody: uri,
+    method: 'post', 
+    onComplete: openId
+    });
+}
+
+function openIdOk(name, email) {
+  
+  if ($('bx_fw[name]').value == '') {
+      $('bx_fw[name]').value = name;
+  }
+  if ($('bx_fw[email]').value == '') {
+      $('bx_fw[email]').value = email;
+  }
+  document.getElementById('verify').setAttribute("value","verified");
+}
+
+function immediate() {
+
+}
