07c5887f0991d0a8f97e7d602db4ad8cb92aa297
chregu <chregu@99f7f646-bdd5-0310-89f9-ddcb10b10bf3>
2006-03-27 18:40:14 +0000

    cache for 3 days
    git-svn-id: https://svn.liip.ch/repos/public/popoon/trunk@6711 99f7f646-bdd5-0310-89f9-ddcb10b10bf3

--- components/serializers/fo2pdf.php
+++ components/serializers/fo2pdf.php
@@ -50,7 +50,7 @@ class popoon_components_serializers_fo2pdf extends popoon_components_serializer 
             
             $this->sc = popoon_helpers_simplecache::getInstance();
             $this->md5 = md5($xml);
-            if ($pdf = $this->sc->simpleCacheCheck($this->md5,"fo2pdf",null,"file",3600 * 24)) {
+            if ($pdf = $this->sc->simpleCacheCheck($this->md5,"fo2pdf",null,"file",3600 * 24 * 3)) {
 	        header("Content-Length: ".filesize($pdf));
                 readfile($pdf);
                 return true;
