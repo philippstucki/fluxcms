4a03d22abd1e085c3f4f01266085ac6ebb265a81
philipp <philipp@99f7f646-bdd5-0310-89f9-ddcb10b10bf3>
2006-05-22 15:03:40 +0000

    - two new fields: date_created and date_changed
    git-svn-id: https://svn.liip.ch/repos/public/fluxcms/trunk@6889 99f7f646-bdd5-0310-89f9-ddcb10b10bf3

---
+++ inc/bx/dbforms2/fields/date/changed.php
@@ -0,0 +1,52 @@
+<?php
+// +----------------------------------------------------------------------+
+// | BxCMS                                                                |
+// +----------------------------------------------------------------------+
+// | Copyright (c) 2001-2006 Bitflux GmbH                                 |
+// +----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or        |
+// | modify it under the terms of the GNU General Public License (GPL)    |
+// | as published by the Free Software Foundation; either version 2       |
+// | of the License, or (at your option) any later version.               |
+// | The GPL can be found at http://www.gnu.org/licenses/gpl.html         |
+// +----------------------------------------------------------------------+
+// | Author: Bitflux GmbH <flux@bitflux.ch>                               |
+// +----------------------------------------------------------------------+
+//
+// $Id$
+
+/**
+ * DOCUMENT_ME
+ *
+ * @package bx_dbforms2
+ * @category 
+ * @author Bitflux GmbH <flux@bitflux.ch>
+ */
+class bx_dbforms2_fields_date_changed extends bx_dbforms2_fields_date {
+    
+    public function __construct($name) {
+        parent::__construct($name);
+        $this->type = 'date_changed';
+        $this->XMLName = 'input';
+    }
+    
+    public function getSQLValue() {
+        if($this->parentForm->queryMode == bx_dbforms2::QUERYMODE_INSERT || $this->parentForm->queryMode == bx_dbforms2::QUERYMODE_UPDATE) {
+            return "NOW()";
+        }
+        
+        return $this->value;
+    }
+    
+    public function quoteSQLValue() {
+        switch($this->parentForm->queryMode) {
+            case bx_dbforms2::QUERYMODE_INSERT:
+            case bx_dbforms2::QUERYMODE_UPDATE:
+                return FALSE;
+        }
+        return TRUE;
+    }
+    
+}
+
+?>
