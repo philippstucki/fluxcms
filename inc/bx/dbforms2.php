e8b37de5595f118bb87f14cc4609cd770a18aacd
philipp <philipp@99f7f646-bdd5-0310-89f9-ddcb10b10bf3>
2006-05-22 13:12:48 +0000

    - class constants for dbforms2
    git-svn-id: https://svn.liip.ch/repos/public/fluxcms/trunk@6876 99f7f646-bdd5-0310-89f9-ddcb10b10bf3

---
+++ inc/bx/dbforms2.php
@@ -0,0 +1,26 @@
+<?php
+// +----------------------------------------------------------------------+
+// | BxCMS                                                                |
+// +----------------------------------------------------------------------+
+// | Copyright (c) 2001-2006 Bitflux GmbH                                 |
+// +----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or        |
+// | modify it under the terms of the GNU General Public License (GPL)    |
+// | as published by the Free Software Foundation; either version 2       |
+// | of the License, or (at your option) any later version.               |
+// | The GPL can be found at http://www.gnu.org/licenses/gpl.html         |
+// +----------------------------------------------------------------------+
+// | Author: Bitflux GmbH <flux@bitflux.ch>                               |
+// +----------------------------------------------------------------------+
+//
+// $Id$
+
+class bx_dbforms2 {
+    
+    const QUERYMODE_SELECT = 1;
+    const QUERYMODE_UPDATE = 2;
+    const QUERYMODE_INSERT = 3;
+    const QUERYMODE_DELETE = 4;
+    
+}
+
