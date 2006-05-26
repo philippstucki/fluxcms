e6383fe03c842156fca5114781dba30b76b3a6e5
chregu <chregu@99f7f646-bdd5-0310-89f9-ddcb10b10bf3>
2006-05-26 08:50:17 +0000

    wrong part
    git-svn-id: https://svn.liip.ch/repos/public/fluxcms/trunk@6905 99f7f646-bdd5-0310-89f9-ddcb10b10bf3

--- admin/webinc/formnavi.php
+++ admin/webinc/formnavi.php
@@ -32,9 +32,9 @@ function changeElement ()
 if (document.forms['bookmarks'].selectElement.selectedIndex > 0) {
 var element = document.forms['bookmarks'].selectElement.options[document.forms['bookmarks'].selectElement.selectedIndex].value;
 if(element.match(/\//)) {
-    parent.edit.location.href="<?php echo BX_WEBROOT; ?>"+element+"/";
+    parent.edit.location.href="<?php echo BX_WEBROOT; ?>"+element;
 } else {
-    parent.edit.location.href="<?php echo BX_WEBROOT; ?>forms/"+element;
+    parent.edit.location.href="<?php echo BX_WEBROOT; ?>forms/"+element+"/";
 }
 document.forms['bookmarks'].selectElement.selectedIndex = 0;
 }
