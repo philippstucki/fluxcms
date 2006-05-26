8eed6b5ebeb7a2f907e348cde33ee93c30dc5aa6
chregu <chregu@99f7f646-bdd5-0310-89f9-ddcb10b10bf3>
2006-05-26 21:09:22 +0000

    udpate date
    git-svn-id: https://svn.liip.ch/repos/public/fluxcms_demo/trunk@6939 99f7f646-bdd5-0310-89f9-ddcb10b10bf3

--- div/db/dbexport.sh
+++ div/db/dbexport.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-mysqldump  --skip-opt --extended-insert=false --compatible=mysql40 -Q --add-drop-table --complete-insert bxcms >  bxcms.sql
+mysqldump  --compatible=mysql40 --skip-opt --extended-insert=false  -Q --add-drop-table --complete-insert bxcms >  bxcms.sql
 
-# Uncomment this to use sed for replacing DEFAULT CHARSET entries
-#sed "s/ DEFAULT CHARSET=latin1//g" ./berghilfe.sql > berghilfe.sql.sed
-#if [ -f "berghilfe.sql.sed" ];then
-#    mv berghilfe.sql.sed berghilfe.sql
-#fi
