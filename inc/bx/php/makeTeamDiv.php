74a236f030c91b4d144f68235f7eb791d521b245
benjamin <benjamin@99f7f646-bdd5-0310-89f9-ddcb10b10bf3>
2006-06-08 12:19:03 +0000

    ajax -> php
    git-svn-id: https://svn.liip.ch/repos/public/fluxcms/trunk@7105 99f7f646-bdd5-0310-89f9-ddcb10b10bf3

---
+++ inc/bx/php/makeTeamDiv.php
@@ -0,0 +1,41 @@
+<?php
+header("Content-type: text/xml");
+mysql_connect('localhost', 'root', '');
+mysql_select_db('bb');
+
+$name = file_get_contents("php://input");
+
+
+?>
+
+
+<!--
+
+<html>
+<head>
+<script type="text/javascript">
+</script>
+</head>
+<body>
+</body>
+</html>
+
+
+
+div class="box">
+<img class="bild" src="/files/images/team/braendli_herbert.jpg" alt="Herbert Brändli" border="0" />
+<h2>Herbert Brändli</h2>
+<h3>Meine Ausbildung und Praxis</h3>
+<p>Lic. oec. publ.<br />
+Eidg. dipl. Pensionsversicherungsexperte<br />
+
+CV als  <a href="../../vorsorgepresse/presse.html" target="_blank">PDF </a></p>
+<h3>Meine Aufgaben</h3>
+<p>Geschäftsführung</p>
+<h3>So erreichen Sie mich</h3>
+<p>Fon 044 724 60 62<br />
+Fax 044 724 60 74<br />
+<img src="/themes/bb-vorsorge/images/kontact.gif" alt="/themes/bb-vorsorge/images/kontact.gif" /> <a href="&#109;&#97;&#105;&#108;&#116;&#111;&#58;h.braendli&#64;bb-vorsorge.ch">Herbert Brändli </a></p> 
+<br class="antifloat" />
+
+</div-->
