2cc3b7b330c6e3ea1130ab4efe11e92695ae1f7f
benjamin <benjamin@99f7f646-bdd5-0310-89f9-ddcb10b10bf3>
2006-06-08 12:26:38 +0000

    update
    git-svn-id: https://svn.liip.ch/repos/public/fluxcms_demo/trunk@7107 99f7f646-bdd5-0310-89f9-ddcb10b10bf3

--- structure/quicklinks.php
+++ structure/quicklinks.php
@@ -3,7 +3,7 @@
 $quicklinks = array (
 'admin/siteoptions/' => $i18n->translate('Site-Options'),
 // 'admin/dbforms2/blogposts/' => 'Blogposts',
-
+ 'admin/dbforms2/team/' => 'B + B Vorsorge-Team',
 'users' => 'Users',
 //'options' => 'options (dbform)',
 );
