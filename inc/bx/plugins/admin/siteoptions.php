0c5d799d6e6318708bd4c2c6e3904c0d8f91271f
chregu <chregu@99f7f646-bdd5-0310-89f9-ddcb10b10bf3>
2006-06-11 09:15:17 +0000

    disable openid and captcha (not really tested)
    git-svn-id: https://svn.liip.ch/repos/public/fluxcms/trunk@7141 99f7f646-bdd5-0310-89f9-ddcb10b10bf3

--- inc/bx/plugins/admin/siteoptions.php
+++ inc/bx/plugins/admin/siteoptions.php
@@ -150,13 +150,15 @@ class bx_plugins_admin_siteoptions extends bx_plugins_admin implements bxIplugin
            'true' => "Yes")
            , $dom,array("help"=>$i18n->translate("help_blogSendRejectedCommentNotification")));
        
+       /*
        $this->addOption("blogCaptchaAfterDays","text",$dom,array("help"=>$i18n->translate("help_blogCaptchaAfterDays")));
-       
+       */
        $this->addOption("blogWeblogsPing","textarea",$dom,array("help"=>$i18n->translate("help_blogWeblogsPing"),"ArrayAsNewline"=>true));
+       /*
        $this->addSelectOption("openIdEnabled",array(
         'false' => "No",
            'true' => "Yes"),$dom,array("help"=>$i18n->translate("help_openIdEnabled")));
-       
+       */
         $this->addOption("copyright","text",$dom,array("help"=>$i18n->translate("help_copyright")));
         
         $this->addOption("cclink","text",$dom,array("help"=>$i18n->translate("help_cclink")));
