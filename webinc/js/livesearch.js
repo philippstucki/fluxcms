55f4bae924c52019f8ac2c3981c8aab11c8e6e3e
chregu <chregu@99f7f646-bdd5-0310-89f9-ddcb10b10bf3>
2006-06-19 11:03:41 +0000

    preview comment code
    git-svn-id: https://svn.liip.ch/repos/public/fluxcms/trunk@7233 99f7f646-bdd5-0310-89f9-ddcb10b10bf3

--- webinc/js/livesearch.js
+++ webinc/js/livesearch.js
@@ -162,3 +162,45 @@ function liveSearchSubmit() {
 	}
 }
 
+
+
+
+/* preview comment code */
+
+
+function preview(request){
+    //
+    var previewNode = $('previewComment');
+
+    if (!previewNode) {
+        var lastComment = $('commentForm');
+        var previewNode = document.createElement("div");
+        previewNode.id = 'previewComment';
+        previewNode.className = 'post_content';
+        previewNode = lastComment.parentNode.insertBefore(previewNode,lastComment.nextSibling);
+
+    }
+    previewNode.innerHTML = request.responseText;
+    location.hash = 'previewComment';
+    //document.getElementById('preview').value =request.responseText;
+}
+
+
+function previewSubmit(test) {
+    var name = document.getElementById('name').value;
+    var mail = document.getElementById('email').value;
+    var uri = document.getElementById('openid_url').value;
+    var text = document.getElementById('comment_text').value;
+
+    var f = document.forms['commentForm'];
+
+    body = "mail=" + encodeURIComponent(mail) + "&uri="+ encodeURIComponent(uri) + "&text="+ encodeURIComponent(text) + "&name="+ encodeURIComponent(name);
+    new ajax (liveSearchRoot + 'inc/bx/php/preview.php', {
+        postBody: body,
+                method: 'post',
+                onComplete: preview
+    });
+
+
+    return false;
+}
