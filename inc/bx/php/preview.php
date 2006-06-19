63fde631af93b715244010147da9905ac358be7f
benjamin <benjamin@99f7f646-bdd5-0310-89f9-ddcb10b10bf3>
2006-06-19 10:02:02 +0000

    preview function for comment
    git-svn-id: https://svn.liip.ch/repos/public/fluxcms/trunk@7230 99f7f646-bdd5-0310-89f9-ddcb10b10bf3

---
+++ inc/bx/php/preview.php
@@ -0,0 +1,12 @@
+<?
+include_once("../../../inc/bx/init.php");
+bx_init::start('conf/config.xml', "../../..");
+
+header("Content-type: text/xml");
+?>
+<span>
+<strong><a href="<? print $_POST['uri']; ?>"><? print $_POST['name'] ?></a> @ now</strong>
+<br/>
+<?php print bx_helpers_string::makeLinksClickable(bx_plugins_blog_handlecomment::cleanUpComment($_POST['text'])); ?>
+</span>
+
