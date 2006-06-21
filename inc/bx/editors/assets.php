6fafea790c1a6c8f41d16fbad3064c367ecee4b5
silvan <silvan@99f7f646-bdd5-0310-89f9-ddcb10b10bf3>
2006-06-21 09:20:21 +0000

    - updates
    git-svn-id: https://svn.liip.ch/repos/public/fluxcms/trunk@7265 99f7f646-bdd5-0310-89f9-ddcb10b10bf3

--- inc/bx/editors/assets.php
+++ inc/bx/editors/assets.php
@@ -2,7 +2,14 @@
 
 class bx_editors_assets extends bx_editor implements bxIeditor {
     
+    private $assetTable = 'assets';
+    
+    private $db = null;
+    
+    
     public function __construct() {
+        $this->assetTable = $GLOBALS['POOL']->config->getTablePrefix().$this->assetTable;
+        $this->db = $GLOBALS['POOL']->db;
     }
      
     public function getDisplayName() {
@@ -16,12 +23,34 @@ class bx_editors_assets extends bx_editor implements bxIeditor {
     public function getEditContentById($id) {
         $content = new DomDocument();
         if ($content instanceof DOMDocument) {
-            $content->load('<assets/>');    
+            $content->loadXML('<assets/>');    
         }
-    
+        
+        $assets = $this->getAssetsById($id);
+        if (is_array($assets)) {
+            bx_helpers_xml::array2Dom($assets, $content, $content->documentElement);
+        }
         
         return $content; 
-    } 
+    }
+    
+    
+    private function getAssetsById($id) {
+        $q = "SELECT id,type,name,value FROM ".$this->assetTable;
+        $q.= " WHERE path='".$id."'";
+        $res = $this->db->query($q);
+        
+        if (!MDB2::isError($res)) {
+            $assets = $res->fetchAll(MDB2_FETCHMODE_ASSOC);
+            
+            if (is_array($assets) && sizeof($assets) > 0) {
+                return $assets;
+            }
+        }
+        
+        return false;
+    }
+    
 }
 
 
