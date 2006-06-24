3af4fc588e418abeba97dd33eb9f3138e968ec23
chregu <chregu@99f7f646-bdd5-0310-89f9-ddcb10b10bf3>
2006-06-24 21:41:15 +0000

    sidebar editor
    git-svn-id: https://svn.liip.ch/repos/public/fluxcms/trunk@7306 99f7f646-bdd5-0310-89f9-ddcb10b10bf3

--- webinc/plugins/blog/sidebar.js
+++ webinc/plugins/blog/sidebar.js
@@ -10,16 +10,19 @@ function initLists() {
 
 
 function initList(nr) {
-	Sortable.create('list'+nr, {ghsting: true,
+	Sortable.create('list'+nr, {
 		dropOnEmpty:true,containment:["list0","list1","list2"],constraint:false,
         onUpdate:function() {
 			//new Effect.Highlight('list'+nr,{});
 			
 			
-            new Ajax.Updater('list-info', 
-			'./', 
+            new Ajax.Request( 
+			'./listupdate/', 
 			{ onComplete: function(request) { 
-				//	new Effect.Highlight('list'+nr,{});
+					new Effect.Opacity('list-info',
+					{ 	duration: 2, 
+						
+					from: 1.0, to: 0 });
 			}, 
 			parameters: serializeList(nr), 
 			evalScripts:false, 
@@ -41,12 +44,97 @@ function serializeList(nr) {
 	}
 	return params;
 	
-	 
+	
 }
 
 
 function editItem(id) {
-	alert(id);
+	
+	var myAjaxObj = new Ajax.Request(
+	'./edit/',
+	{
+		
+		parameters: 'id='+ id,
+		onComplete: showEdit,
+		
+	}
+	);
+	
 	return false;
+}
+
+function showEdit(request) {
+	var data = eval("(" + request.responseText.replace(/<\?xml.*?>/,"") + ")");
+	
+	$('sidebar_edit').style.display="block";
+	
+	$('name').value = data.name;
+	$('content').value = data.content;
+	$('id').value = data.id;
+}
+
+
+function sendEdit() {
+	var myAjaxObj = new Ajax.Request(
+	
+	'./sendedit/',
+	{
+		
+		parameters: Form.serialize( $('editform')),
+		onComplete: editCompleted
+	}
+	);
+	
+	return false;
+}
+
+function createNew() {
+	$('sidebar_edit').style.display="block";
+	$('name').value = "noname";
+	$('content').value = "";
+	$('id').value = "";
+}
+
+
+function editCompleted(request) {
+	
+	var data = eval("(" + request.responseText.replace(/<\?xml.*?>/,"") + ")");
+	
+	var listId = "list0"; 
+	if ($('item_'+data.id)) {
+		var li = $('item_'+data.id);
+		var listId = li.parentNode.id;
+		li.innerHTML = data.content;
+	} else {
+		$('id').value = data.id;
+		var li = $(listId).appendChild(document.createElement("li"));
+		li.id = 'item_'+data.id;
+		li.innerHTML = data.content;
+		initList(0);
+	}
+	
+}
+
+function deleteEntry() {
+	
+	var myAjaxObj = new Ajax.Request(
+	
+	'./delete/',
+	{
+		
+		parameters: "bx[plugins][admin_edit][delete]="+ $('id').value,
+		onComplete: deleteCompleted
+	}
+	);
+	
+}
+
+function deleteCompleted(request) {
+	var data = eval("(" + request.responseText.replace(/<\?xml.*?>/,"") + ")");
+	
+	var li = $('item_'+data.id);
+	if (li) {
+		li.parentNode.removeChild(li);
+	}
 }
 
