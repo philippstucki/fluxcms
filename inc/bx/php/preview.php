d2a01e7cff49cb7ca13044b3d607716bd4e32f1a
chregu <chregu@99f7f646-bdd5-0310-89f9-ddcb10b10bf3>
2006-07-02 15:16:50 +0000

    use blog.xsl
    git-svn-id: https://svn.liip.ch/repos/public/fluxcms/trunk@7403 99f7f646-bdd5-0310-89f9-ddcb10b10bf3

--- inc/bx/php/preview.php
+++ inc/bx/php/preview.php
@@ -3,10 +3,28 @@ include_once("../../../inc/bx/init.php");
 bx_init::start('conf/config.xml', "../../..");
 
 header("Content-type: text/xml");
+
+
+$xslt = new xsltprocessor();
+$xml = file_get_contents(BX_LIBS_DIR.'plugins/blog/preview.xml');
+$_POST['text'] = bx_helpers_string::makeLinksClickable(bx_plugins_blog_handlecomment::cleanUpComment($_POST['text']));
+
+$xmls = str_replace(array("{{name}}","{{text}}","{{url}}"),array($_POST['name'],$_POST['text'],$_POST['name']),$xml);
+$xml = new domdocument();
+$xml->loadXML($xmls);
+libxml_use_internal_errors(true);
+
+$xsls = file_get_contents(BX_LIBS_DIR.'plugins/blog/preview.xsl');
+
+
+$xsls = str_replace('{{blog.xsl}}',BX_THEMES_DIR.$GLOBALS['POOL']->config->theme. '/blog.xsl',$xsls);
+
+$xsl = new domdocument();
+$xsl->loadXML($xsls);
+$xslt->importStylesheet($xsl);
+
+$xslt->registerPHPFunctions();
+print $xslt->transformToXML($xml);
+
 ?>
-<span>
-<strong><a href="<? print $_POST['uri']; ?>"><? print $_POST['name'] ?></a> @ now</strong>
-<br/>
-<?php print bx_helpers_string::makeLinksClickable(bx_plugins_blog_handlecomment::cleanUpComment($_POST['text'])); ?>
-</span>
 
