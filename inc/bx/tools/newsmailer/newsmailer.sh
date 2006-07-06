e563c9c369b32fd55fef348240cc5a986b273c6c
chregu <chregu@99f7f646-bdd5-0310-89f9-ddcb10b10bf3>
2006-07-06 10:00:30 +0000

    no paths here
    git-svn-id: https://svn.liip.ch/repos/public/fluxcms/trunk@7456 99f7f646-bdd5-0310-89f9-ddcb10b10bf3

--- inc/bx/tools/newsmailer/newsmailer.sh
+++ inc/bx/tools/newsmailer/newsmailer.sh
@@ -1,6 +1,5 @@
 #/bin/bash
 
-cd /var/www/kolibri-seminare.ch/
 
 /usr/local/bin/php inc/bx/tools/newsmailer/newsmailer.php preparemails
 /usr/local/bin/php inc/bx/tools/newsmailer/newsmailer.php sendmails
