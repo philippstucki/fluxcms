7609a4f89cda30bdad76976ac369b371843f04ab
benjamin <benjamin@99f7f646-bdd5-0310-89f9-ddcb10b10bf3>
2006-07-20 14:21:34 +0000

    helper to collect and count tags from blog
    git-svn-id: https://svn.liip.ch/repos/public/fluxcms/trunk@7616 99f7f646-bdd5-0310-89f9-ddcb10b10bf3

---
+++ inc/bx/helpers/tags.php
@@ -0,0 +1,30 @@
+<?php
+// 
+// add this to blog.xsl:     
+// <xsl:template match="tags" mode="xhtml">
+// <h3>Tags</h3>
+// <xsl:copy-of select="php:functionString('bx_helpers_tags::getTags', @entries, $collectionUri)"/>
+// </xsl:template>
+// 
+// and this to the sidebar editor. 
+// Name is tags
+// and content is (0 == nothing will be displayed)
+// <tags entries="10"/> 
+// 
+class bx_helpers_tags {
+    
+    public static function getTags($entries, $path) {
+        $tableprefix = $GLOBALS['POOL']->config->getTableprefix();
+        $query = "select DISTINCT count(tag) as c, tag from ".$tableprefix."tags left join ".$tableprefix."properties2tags on ".$tableprefix."tags.id = ".$tableprefix."properties2tags.tag_id left join ".$tableprefix."blogposts on ".$tableprefix."properties2tags.path = concat('".$blogpath."',".$tableprefix."blogposts.post_uri,'.html') group by tag order by c Desc LIMIT ".$entries;
+        $res = $GLOBALS['POOL']->db->query($query);
+        $text = "<ul>";
+        while ($row = $res->fetchRow(MDB2_FETCHMODE_ASSOC)) {
+                $text .= "<li><a rel='tag' href='".BX_WEBROOT_W.$path."archive/tag/".$row['tag']."/'>".$row['tag']." (".$row['c'].")</a></li>\n";
+        }
+        
+        $text .= "</ul>";
+        $xml = new DomDocument();
+        $xml->loadXML($text);
+        return $xml;
+    }
+}
