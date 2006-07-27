1078659f63982712f58990d175afd877bb3d0026
philipp <philipp@99f7f646-bdd5-0310-89f9-ddcb10b10bf3>
2006-07-27 09:03:27 +0000

    fieldType is not used anywhere...
    git-svn-id: https://svn.liip.ch/repos/public/fluxcms/trunk@7716 99f7f646-bdd5-0310-89f9-ddcb10b10bf3

--- inc/bx/dbforms2/field.php
+++ inc/bx/dbforms2/field.php
@@ -251,9 +251,6 @@ class bx_dbforms2_field {
             'name' => $this->name,
             'descr' => $this->attributes['descr'],
             'id' => $this->parentForm->name.'_'.$this->name,
-            
-            // FIXME: isn't this the same as the above attribute 'type' ?
-            'fieldType' =>  str_replace('bx_dbforms2_fields_', '', get_class($this)),
         );
         
         if (isset($this->attributes['onkeyup'])) {
