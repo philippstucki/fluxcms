df31ed65619f57f64e57dfcef415e8700a083946
benjamin <benjamin@99f7f646-bdd5-0310-89f9-ddcb10b10bf3>
2006-08-21 06:48:14 +0000

    new user administration
    git-svn-id: https://svn.liip.ch/repos/public/fluxcms/trunk@7797 99f7f646-bdd5-0310-89f9-ddcb10b10bf3

--- inc/bx/plugins/admin/users.php
+++ inc/bx/plugins/admin/users.php
@@ -27,14 +27,41 @@ class bx_plugins_admin_users extends bx_plugins_admin implements bxIplugin  {
     }
     
     public function getContentById($path, $id) {
-		$xml = $this->getUsers($id);
-		return $xml;
+		$tablePrefix =  $GLOBALS['POOL']->config->getTablePrefix();
+		
+		$xml = $this->getUsers($id, $tablePrefix);
+		$dom = new domDocument();
+		$dom->loadXML($xml);
+		
+		return $dom;
     }
     
-    protected function getUsers($id) {
+    protected function getUsers($id, $tablePrefix) {
 		
-		if(isset($_GET['id'])) {
+		$services = bx_helpers_perm::getAuthServices();
+		$xml = "<useradministration>";
+		$xml .= "<authservices>";
+		foreach($services as $service) {
+			$xml .= "<authservice>".$service."</authservice>";
+		}
+		$xml .= "</authservices>";
+		
+		//removes a users
+		if(isset($_GET['delete']) && $_GET['delete']) {
+			$user_del = $_GET['delete'];
+		}
+		
+		if(isset($user_del)) {
+			$query = "delete from ".$tablePrefix."users where id = ".$user_del;
+			$GLOBALS['POOL']->db->query($query);
+		}
+		
+		if(isset($_GET['id']) && $_GET['id']) {
 			$user_id = $_GET['id'];
+		}
+		
+		if(isset($_GET['add']) && $_GET['add']) {
+			$user_add = $_GET['add'];
 		}
 		
 		$i18n = $GLOBALS['POOL']->i18nadmin;
@@ -43,10 +70,12 @@ class bx_plugins_admin_users extends bx_plugins_admin implements bxIplugin  {
 		if($id == "/edit/" and isset($user_id)) {
 			
 			$query = "select * from ".$tablePrefix."users where ID =".$user_id;
+				
 			$res = $GLOBALS['POOL']->db->query($query);
+			
+			
 			$row = $res->fetchRow(MDB2_FETCHMODE_ASSOC);
 			
-			$xml = "<user>";
 			$xml .= "<user>";
 			$xml .= "<username>".$row['user_login']."</username>";
 			$xml .= "<fullname>".$row['user_fullname']."</fullname>";
@@ -57,34 +86,59 @@ class bx_plugins_admin_users extends bx_plugins_admin implements bxIplugin  {
 			$xml .= "<user_adminlang>".$row['user_adminlang']."</user_adminlang>";
 			$xml .= "<plazes_username>".$row['plazes_username']."</plazes_username>";
 			$xml .= "<plazes_password>".$row['plazes_password']."</plazes_password>";
-			$xml .= "</user>";
+			
+			//reads services fromd atabase
+			$query_authservices = "select * from ".$tablePrefix."userauthservices where user_id = '".$user_id."'";
+			
+			$res_services = $GLOBALS['POOL']->db->query($query_authservices);
+			
+			$xml .= "<services>";
+			while($row = $res_services->fetchRow(MDB2_FETCHMODE_ASSOC)) {
+				$xml .= "<service>";
+				$xml .= "<id>".$row['id']."</id>";
+				$xml .= "<user_id>".$row['user_id']."</user_id>";
+				$xml .= "<servicename>".$row['service']."</servicename>";
+				$xml .= "<account>".$row['account']."</account>";
+				$xml .= "</service>";
+			}
+			$xml .= "</services>";
+			
 			$xml .= "</user>";
 			
 		} else {
-			$query = "select user_login, user_fullname, user_email, ID from ".$tablePrefix."users";
-			$res = $GLOBALS['POOL']->db->query($query);
-			
-			$xml = "<users>";
-			
-			while($row = $res->fetchRow(MDB2_FETCHMODE_ASSOC)) {
-				$xml .= "<user>";
-				$xml .= "<username>".$row['user_login']."</username>";
-				$xml .= "<fullname>".$row['user_fullname']."</fullname>";
-				$xml .= "<mail>".$row['user_email']."</mail>";
-				$xml .= "<id>".$row['id']."</id>";
-				$xml .= "</user>";
+			if($id == "/edit/" and isset($user_add)) {
+				$xml .= "<new/>";
+			} else {
+				$query = "select user_login, user_fullname, user_email, ID from ".$tablePrefix."users";
+				
+				$res = $GLOBALS['POOL']->db->query($query);
+				
+				$xml .= "<users>";
+				
+				while($row = $res->fetchRow(MDB2_FETCHMODE_ASSOC)) {
+					$xml .= "<user>";
+					$xml .= "<username>".$row['user_login']."</username>";
+					$xml .= "<fullname>".$row['user_fullname']."</fullname>";
+					$xml .= "<mail>".$row['user_email']."</mail>";
+					$xml .= "<id>".$row['id']."</id>";
+					$xml .= "</user>";
+				}
+				
+				$xml .= "</users>";
 			}
-			
-			$xml .= "</users>";
 		}
-		$dom = new domDocument();
-		$dom->loadXML($xml);
-		return $dom;
+		$xml .= "</useradministration>";
+		return $xml;
 	}
     
     /* FIXME:: this should be cleaned up. arguments are $path,$id,$data,$mode */
     public function handlePost($path, $name, $ext, $data=null) {
-        $perm = bx_permm::getInstance();
+        $tablePrefix =  $GLOBALS['POOL']->config->getTablePrefix();
+		$perm = bx_permm::getInstance();
+		$active_user_id = $_SESSION['_authsession']['data']['id'];
+		$services = bx_helpers_perm::getAuthServices();
+		
+		
 		if (!$perm->isAllowed('/permissions/',array('permissions-back-users'))) {
     		throw new BxPageNotAllowedException();
     	}
@@ -93,16 +147,64 @@ class bx_plugins_admin_users extends bx_plugins_admin implements bxIplugin  {
             $data = $_REQUEST['bx']['plugins']['admin_users'];
             $data = bx_helpers_globals::stripMagicQuotes($data);
         }
-        
-		if(isset($_GET['id'])) {
+		if(isset($_GET['id']) && $_GET['id']) {
 			$user_id = $_GET['id'];
 		}
 		
-		$tablePrefix =  $GLOBALS['POOL']->config->getTablePrefix();
-        //$query = "insert into ".$tablePrefix."users (user_login, user_fullname, user_email, user_gupi, user_gid, user_adminlang, plazes_username, plazes_password) VALUES('".$data['username']."','".$data['fullname']."','".$data['mail']."','".$data['gupi']."','".$data['gid']."','".$data['lang']."','".$data['plazes_username']."','".$data['plazes_pwd']."') where id = ".$user_id;
-		$query = "update ".$tablePrefix."users SET user_login = '".$data['username']."', user_fullname = '".$data['fullname']."', user_email = '".$data['mail']."', user_gupi = '".$data['gupi']."', user_gid = '".$data['gid']."', user_adminlang = '".$data['lang']."', plazes_username = '".$data['plazes_username']."', plazes_password = '".$data['plazes_pwd']."' where id = ".$user_id;
-		$res = $GLOBALS['POOL']->db->query($query);
-	}	
+		$pwd_query = "select user_pass from ".$tablePrefix."users where id = '".$active_user_id."'";
+		$res = $GLOBALS['POOL']->db->query($pwd_query);
+		$re = $res->fetchRow(MDB2_FETCHMODE_ASSOC);
+		if($re['user_pass'] == md5($data['pwd'])) {
+			if(isset($user_id)) {
+				if(isset($data['new_pwd']) && $data['new_pwd'] && isset($data['new_pwd_re']) && $data['new_pwd_re']) {
+					if($data['new_pwd'] == $data['new_pwd_re']) {
+						$query = "update ".$tablePrefix."users SET user_login = '".$data['username']."', user_fullname = '".$data['fullname']."', user_email = '".$data['mail']."', user_gupi = '".$data['gupi']."', user_gid = '".$data['gid']."', user_adminlang = '".$data['lang']."', plazes_username = '".$data['plazes_username']."', plazes_password = '".$data['plazes_pwd']."' , user_pass = '".md5($data['new_pwd'])."' where id = ".$user_id;
+					} else {
+						print "<p style='color:red'>New password didn't match try again please</p>";
+					}
+				}
+				
+				//update  services
+				foreach($services as $service) {
+					if(isset($data[$service]) && $data[$service]) {
+						$max_id_query = "select MAX(id) as last_id from ".$tablePrefix."users";
+						$result = $GLOBALS['POOL']->db->query($max_id_query);
+						$insert_id = $result->fetchRow(MDB2_FETCHMODE_ASSOC);
+						$userauthservices_query = "update ".$tablePrefix."userauthservices set service = '".$service."', account = '".$data[$service]."' where user_id = '".$user_id."' and service = '".$service."'";
+						$res = $GLOBALS['POOL']->db->query($userauthservices_query);
+					}
+				}
+				
+				if(!isset($query)) {
+					$query = "update ".$tablePrefix."users SET user_login = '".$data['username']."', user_fullname = '".$data['fullname']."', user_email = '".$data['mail']."', user_gupi = '".$data['gupi']."', user_gid = '".$data['gid']."', user_adminlang = '".$data['lang']."', plazes_username = '".$data['plazes_username']."', plazes_password = '".$data['plazes_pwd']."' , user_pass = '".$data['new_pwd']."' where id = ".$user_id;		
+				}
+				if(isset($query)) {
+					$res = $GLOBALS['POOL']->db->query($query);
+				}
+			}
+		
+			if(isset($data['add']) && $data['add']) {
+				$query = "insert into ".$tablePrefix."users (user_login, user_fullname, user_email, user_gupi, user_gid, user_adminlang, plazes_username, plazes_password, user_pass) value('".$data['username']."', '".$data['fullname']."', '".$data['mail']."', '".$data['gupi']."', '".$data['gid']."', '".$data['lang']."', '".$data['plazes_username']."', '".$data['plazes_pwd']."', '".md5($data['new_pwd'])."')";
+				
+				if(isset($query)) {
+					$res = $GLOBALS['POOL']->db->query($query);
+				}
+				
+				//insert services
+				foreach($services as $service) {
+					if(isset($data[$service]) && $data[$service]) {
+						$max_id_query = "select MAX(id) as last_id from ".$tablePrefix."users";
+						$result = $GLOBALS['POOL']->db->query($max_id_query);
+						$insert_id = $result->fetchRow(MDB2_FETCHMODE_ASSOC);
+						$userauthservices_query = "insert into ".$tablePrefix."userauthservices (user_id, service, account) value('".$insert_id['last_id']."', '".$service."', '".$data[$service]."')";
+						$res = $GLOBALS['POOL']->db->query($userauthservices_query);
+					}
+				}
+			}
+		} else {
+			print "<p style='color:red'>You had a misstake in your password</p>";
+		}
+	}
     protected function getParentCollection($path) {
         $parent = dirname($path);
         if ($parent != "/"){
