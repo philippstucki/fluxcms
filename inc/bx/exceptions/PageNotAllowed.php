b40bb6708462faec9779be6de803d4247b6bd2c3
chregu <chregu@99f7f646-bdd5-0310-89f9-ddcb10b10bf3>
2006-12-14 10:42:18 +0000

    keep the errormessage for the web login
    git-svn-id: https://svn.liip.ch/repos/public/fluxcms/trunk@8307 99f7f646-bdd5-0310-89f9-ddcb10b10bf3

--- inc/bx/exceptions/PageNotAllowed.php
+++ inc/bx/exceptions/PageNotAllowed.php
@@ -5,8 +5,12 @@ class BxPageNotAllowedException extends Exception {
         if (!$uri) {
             $uri = substr($_SERVER['REQUEST_URI'],1);
         }
+        $uri = preg_replace("#.loginerror=1#",'',$uri);
         $this->message = "$uri is not allowed to be viewed.";
         parent::__construct();
+        if (!empty($_GET) && isset($_GET['loginerror'])) {
+            $this->userInfo =  $GLOBALS['POOL']->i18nadmin->translate('Wrong login or password.');
+        }
     }
 }
 ?>
