888b168f5c59d8c4758bd32fc6177a3082badbcb
benjamin <benjamin@99f7f646-bdd5-0310-89f9-ddcb10b10bf3>
2007-01-10 12:28:33 +0000

    tagcloud plugin (only work with the blog atm)
    git-svn-id: https://svn.liip.ch/repos/public/fluxcms/trunk@8370 99f7f646-bdd5-0310-89f9-ddcb10b10bf3

---
+++ inc/bx/plugins/tagcloud.php
@@ -0,0 +1,109 @@
+<?php
+
+/*
+
+<?xml version="1.0"?>
+<bxcms xmlns="http://bitflux.org/config">
+
+    <plugins>
+        <parameter name="xslt" type="pipeline" value="tagcloud.xsl"/>
+<plugin type="navitree">
+        </plugin>
+        <plugin type="tagcloud">
+            <!-- path to the blog -->
+            <parameter name="locations" value="/blog/"/>
+            <!-- max fontsize -->
+            <parameter name="fontsize" value="36"/>
+        </plugin>
+    </plugins>
+</bxcms>
+
+*/
+
+class bx_plugins_tagcloud extends bx_plugin implements bxIplugin {
+    
+    static public $instance = array();
+    protected $res = array();
+    
+    protected $db = null;
+    protected $tablePrefix = null;
+    
+    public static function getInstance($mode) {
+        
+        if (!isset(self::$instance[$mode])) {
+            self::$instance[$mode] = new bx_plugins_tagcloud($mode);
+        } 
+        return self::$instance[$mode];
+    }
+    
+    protected function __construct($mode) {
+        $this->tablePrefix = $GLOBALS['POOL']->config->getTablePrefix();
+        $this->db = $GLOBALS['POOL']->db;
+        $this->mode = $mode;
+    }
+    
+    public function isRealResource($path , $id) {
+        return true;
+    }
+    
+    public function getIdByRequest($path, $name = NULL, $ext = NULL) {
+        
+        return $name.'.'.$this->name;
+        
+    }
+    
+    public function getContentById($path, $id){
+        $tags = array();
+        $locations = $this->getParameter($path,"locations");
+        
+        $query="select count(fluxcms_tags.tag) as tagcount, tag from fluxcms_tags left join fluxcms_properties2tags on fluxcms_properties2tags.tag_id = fluxcms_tags.id where fluxcms_tags.id <> '11' and fluxcms_properties2tags.path like '/blog/%' group by fluxcms_tags.tag";
+        $res = $GLOBALS['POOL']->db->query($query);
+        
+        while($row = $res->fetchAll(MDB2_FETCHMODE_ASSOC)) {
+            $tags = $row;
+        }
+        
+        $max = $this->maxof($tags);
+        
+        $res = $GLOBALS['POOL']->db->query($query);
+        $xml = "<tagcloud>";
+        while($row = $res->fetchRow(MDB2_FETCHMODE_ASSOC)) {
+            $xml .= "<tag>";
+            $xml .= "<name>".$row['tag']."</name>";
+            $size = $this->getFontSize($row['tagcount'], $max, $path);
+            $xml .= "<size>".$size."</size>";
+            $xml .= "<path>".$locations."</path>";
+            $xml .= "</tag>";
+        }
+        $xml .= "</tagcloud>";
+        
+        $dom = new DomDocument();
+        $dom->loadXML($xml);
+        return $dom;
+        
+    }
+    
+    public function getFontSize($count, $max, $path) {
+        $maxFontSize = $this->getParameter($path,"fontsize");
+        $percent = $count / $max * 100;
+        $size = $percent / 100 * $maxFontSize;
+        return round($size);
+    }
+    
+    public function maxof($array) {
+         $max = 0;
+         foreach($array as $element) {
+             if ($element['tagcount'] > $max) {
+                 $max = $element['tagcount'];
+             }
+             
+         }
+         return $max;
+    }
+    
+    public function adminResourceExists($path, $id, $ext=null, $sample = false) {
+        return true;
+    }
+    
+}
+?>
