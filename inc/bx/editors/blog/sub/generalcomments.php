3bd620a9409f1cd6bf4cffe07e1f36643f803af8
benjamin <benjamin@99f7f646-bdd5-0310-89f9-ddcb10b10bf3>
2007-02-14 07:06:35 +0000

    generalcomments update
    git-svn-id: https://svn.liip.ch/repos/public/fluxcms/trunk@8458 99f7f646-bdd5-0310-89f9-ddcb10b10bf3

--- inc/bx/editors/blog/sub/generalcomments.php
+++ inc/bx/editors/blog/sub/generalcomments.php
@@ -3,7 +3,7 @@
 class bx_editors_blog_sub_generalcomments extends bx_editors_blog_sub {
     
     static protected $id = FALSE;
-	static protected $parent;
+    static protected $parent;
     static protected $timezone = null;
         
     public function getInstance() {
@@ -16,100 +16,99 @@ class bx_editors_blog_sub_generalcomments extends bx_editors_blog_sub {
     }
     
     public function getEditContentById($id) {
-		
-		$tablePrefix = $GLOBALS['POOL']->config->getTablePrefix();
-		
+        $tablePrefix = $GLOBALS['POOL']->config->getTablePrefix();
+        
         $parts =  bx_collections::getCollectionAndFileParts($path, "output");
         $p = $parts['coll']->getFirstPluginMapByRequest("index","html");
         $p = $p['plugin'];
         $colluri = $parts['coll']->uri;
         $blogid =  $p->getParameter($colluri,"blogid");
         
-		if(isset($_GET['id'])) {
-			return self::getEditComment($tablePrefix, $_GET['id']);
-		} else {
-			return self::getLatestComments($tablePrefix, $blogid);
+        if(isset($_GET['id'])) {
+            return self::getEditComment($tablePrefix, $_GET['id']);
+        } else {
+            return self::getLatestComments($tablePrefix, $blogid);
         }
-		/*
-		$query = "SELECT * FROM ".$this->tablePrefix."comments AS generalcomments";
+        /*
+        $query = "SELECT * FROM ".$this->tablePrefix."comments AS generalcomments";
         return bx_helpers_db2xml::getXMLByQuery($query, TRUE);
-		
-		
+        
+        
         if(self::$id == FALSE) 
             self::$id = isset($_GET['id']) ? $_GET['id'] : 0;
-		return $this->getCommentData(self::$id);*/
+        return $this->getCommentData(self::$id);*/
     }
 
     public function handlePOST($path, $id, $data) {
-		$tablePrefix = $GLOBALS['POOL']->config->getTablePrefix();
-		if($data["deletecomments"]) {
-			foreach($data["deletecomments"] as $name => $value) {
-				if(isset($name) && $name != NULL) {
-					$this->deleteComment($name);
-				}
-			}
-		}
-		if(isset($data['comment_id'])) {
+        $tablePrefix = $GLOBALS['POOL']->config->getTablePrefix();
+        if($data["deletecomments"]) {
+            foreach($data["deletecomments"] as $name => $value) {
+                if(isset($name) && $name != NULL) {
+                    $this->deleteComment($name);
+                }
+            }
+        }
+        if(isset($data['comment_id'])) {
             $id = (int) $data['comment_id'];
             $quoted = $this->quotePostData($data);
             
             if($id != 0) {
                 //$query = $this->getUpdateQuery('comments', $quoted, array('comment_author', 'comment_author_email','comment_author_url','comment_status', 'comment_content','comment_date','comment_author_ip'), $id);
-				$query = "update ".$tablePrefix."comments set comment_author = '".$data['comment_author']."', comment_author_email='".$data['comment_author_email']."',comment_author_url='".$data['comment_author_url']."',comment_status='".$data['comment_status']."', comment_content='".$data['comment_content']."',comment_date='".$data['comment_date']."',comment_author_ip='".$data['comment_author_ip']."' where id = '".$id."'";
-				$res = $this->dbwrite->query($query);
-				if (MDB2::isError($res)) {
-					throw new PopoonDBException($res);
-				}
-			}
+                $query = "update ".$tablePrefix."comments set comment_author = '".$data['comment_author']."', comment_author_email='".$data['comment_author_email']."',comment_author_url='".$data['comment_author_url']."',comment_status='".$data['comment_status']."', comment_content='".$data['comment_content']."',comment_date='".$data['comment_date']."',comment_author_ip='".$data['comment_author_ip']."' where id = '".$id."'";
+                $res = $this->dbwrite->query($query);
+                if (MDB2::isError($res)) {
+                    throw new PopoonDBException($res);
+                }
+            }
         }
         
     }
 /*
     protected function getCommentData($id) {
-		$query = "SELECT * FROM ".$this->tablePrefix."comments AS generalcomments WHERE id = '$id'";
+        $query = "SELECT * FROM ".$this->tablePrefix."comments AS generalcomments WHERE id = '$id'";
         return bx_helpers_db2xml::getXMLByQuery($query, TRUE);
     }
-	*/
-	static protected function deleteComment($id) {
-		$tablePrefix = $GLOBALS['POOL']->config->getTablePrefix();
-		$query = "delete from ".$tablePrefix."comments where id =".$id;
-		
-		$res = $GLOBALS['POOL']->db->query($query);
+    */
+    static protected function deleteComment($id) {
+        $tablePrefix = $GLOBALS['POOL']->config->getTablePrefix();
+        $query = "delete from ".$tablePrefix."comments where id =".$id;
+        
+        $res = $GLOBALS['POOL']->db->query($query);
         if (MDB2::isError($res)) {
             throw new PopoonDBException($res);
         }
-	}
-	
-	static protected function getEditComment($tablePrefix = null, $commentid) {
+    }
     
-		$query = "select * from ".$tablePrefix."comments where id = ".$commentid;
-		$res = $GLOBALS['POOL']->db->query($query);
+    static protected function getEditComment($tablePrefix = null, $commentid) {
+        $query = "select * from ".$tablePrefix."comments where id = ".$commentid;
+        $res = $GLOBALS['POOL']->db->query($query);
         if (MDB2::isError($res)) {
             throw new PopoonDBException($res);
         }
-		
-		$row = $res->fetchRow(MDB2_FETCHMODE_ASSOC);
-		
-		$dom = new domDocument();
-		
-		$xml = '<comment>';
-		$xml .= "<comment edit='true'>";
-		$xml .= "<comment_author>".$row['comment_author']."</comment_author>";
-		$xml .= "<comment_author_url>".$row['comment_author_url']."</comment_author_url>";
-		$xml .= "<comment_author_email>".$row['comment_author_email']."</comment_author_email>";
-		$xml .= "<comment_status>".$row['comment_status']."</comment_status>";
-		$xml .= "<comment_content>".$row['comment_content']."</comment_content>";
-		$xml .= "<comment_author_ip>".$row['comment_author_ip']."</comment_author_ip>";
-		$xml .= "<comment_date>".$row['comment_date']."</comment_date>";
-		$xml .= "<comment_id>".$row['id']."</comment_id>";
-		$xml .= "</comment>";
-		$xml .= '</comment>';
-		$dom->loadXML($xml);
-		
-		return $dom;
-	}
-	
-	static protected function getLatestComments($tablePrefix = null, $blogid) {
+        
+        $row = $res->fetchRow(MDB2_FETCHMODE_ASSOC);
+        
+        $dom = new domDocument();
+        
+        $xml = '<comment>';
+        $xml .= "<comment edit='true'>";
+        $xml .= "<comment_author>".$row['comment_author']."</comment_author>";
+        $xml .= "<comment_author_url>".$row['comment_author_url']."</comment_author_url>";
+        $xml .= "<comment_author_email>".$row['comment_author_email']."</comment_author_email>";
+        $xml .= "<comment_status>".$row['comment_status']."</comment_status>";
+        $xml .= "<comment_content>".$row['comment_content']."</comment_content>";
+        $xml .= "<comment_author_ip>".$row['comment_author_ip']."</comment_author_ip>";
+        $xml .= "<comment_date>".$row['comment_date']."</comment_date>";
+        $xml .= "<comment_id>".$row['id']."</comment_id>";
+        $xml .= "</comment>";
+        $xml .= '</comment>';
+        $dom->loadXML($xml);
+        
+        return $dom;
+    }
+    
+    static protected function getLatestComments($tablePrefix = null, $blogid) {
+        bx_helpers_debug::webdump('here');
         
         $gmnow = gmdate("Y-m-d H:i:00",time() + 60);
         if (!$tablePrefix) {
@@ -142,23 +141,24 @@ class bx_editors_blog_sub_generalcomments extends bx_editors_blog_sub {
         }
         
         $query .= " order by comment_date desc limit 10";
-		$res = $GLOBALS['POOL']->db->query($query);
+        bx_helpers_debug::webdump($query);
+        $res = $GLOBALS['POOL']->db->query($query);
         if (MDB2::isError($res)) {
             throw new PopoonDBException($res);
         }
-		
-		$dom = new domDocument();
-		
-		$xml = '<comments>';
-		$xml .= self::getCommentXML($res);
-		$xml .= '</comments>';
-		
-		$dom->loadXML($xml);
-		
+        
+        $dom = new domDocument();
+        
+        $xml = '<comments>';
+        $xml .= self::getCommentXML($res);
+        $xml .= '</comments>';
+        
+        $dom->loadXML($xml);
+        
         return $dom;
     }
-	
-	static protected function getCommentXML($res) {
+    
+    static protected function getCommentXML($res) {
         $xml = '';
         $lastModified = 0;
         while($row = $res->fetchRow(MDB2_FETCHMODE_ASSOC)) {
