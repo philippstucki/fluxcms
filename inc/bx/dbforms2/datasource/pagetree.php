001d8da4344e4966633796d9b0c96a3f3de840ef
chregu <chregu@99f7f646-bdd5-0310-89f9-ddcb10b10bf3>
2007-05-11 13:32:57 +0000

    added new pagetree datasource
    git-svn-id: https://svn.liip.ch/repos/public/fluxcms/trunk@8641 99f7f646-bdd5-0310-89f9-ddcb10b10bf3

---
+++ inc/bx/dbforms2/datasource/pagetree.php
@@ -0,0 +1,114 @@
+<?php
+// +----------------------------------------------------------------------+
+// | BxCMS                                                                |
+// +----------------------------------------------------------------------+
+// | Copyright (c) 2001-2006 Bitflux GmbH                                 |
+// +----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or        |
+// | modify it under the terms of the GNU General Public License (GPL)    |
+// | as published by the Free Software Foundation; either version 2       |
+// | of the License, or (at your option) any later version.               |
+// | The GPL can be found at http://www.gnu.org/licenses/gpl.html         |
+// +----------------------------------------------------------------------+
+// | Author: Bitflux GmbH <flux@bitflux.ch>                               |
+// +----------------------------------------------------------------------+
+//
+// $Id: field.php 4831 2005-06-30 14:41:20Z philipp $
+
+
+/*
+example:
+
+     <dbform:field name="uri" descr="URI" type="select" default="">
+            <dbform:datasource type="pagetree" subtree="/produkte/"></dbform:datasource>
+        </dbform:field>
+       
+*/
+
+/**
+ * DOCUMENT_ME
+ *
+ * @package bx_dbforms2
+ * @category 
+ * @author Bitflux GmbH <flux@bitflux.ch>
+ */
+class bx_dbforms2_datasource_pagetree {
+    
+    /**
+     *  DOCUMENT_ME
+     *  @var var
+     */
+    static private $instance = null;
+    
+    /**
+     *  DOCUMENT_ME
+     *
+     *  @param  type  $var descr
+     *  @access public
+     *  @return type descr
+     */
+    private function __construct() {
+        $this->db = $GLOBALS['POOL']->db;
+    }
+    
+    /**
+     *  DOCUMENT_ME
+     *
+     *  @param  type  $var descr
+     *  @access public
+     *  @return type descr
+     */
+    public static function getInstance() {
+        if (!isset(self::$instance)) {
+            self::$instance = new bx_dbforms2_datasource_pagetree();
+        } 
+        return self::$instance;
+    }
+    
+    
+    /**
+     *  DOCUMENT_ME
+     *
+     *  @param  type  $var descr
+     *  @access public
+     *  @return type descr
+     */
+    public function getValues($args, $childNode) {
+        $prefx = (isset($args['tablePrefix']) && !empty($args['tablePrefix'])) ? $args['tablePrefix'] : $GLOBALS['POOL']->config->getTablePrefix();        
+        $sql = 'select path  from ' . $prefx .'properties where ';
+        
+        if (isset($args['where'])) {
+           $sql .= $args['where']. " AND ";  
+        }
+        
+        if (isset($args['subtree'])) {
+              $sql .= ' path like "'.$args['subtree'] .'%" AND ';
+        }
+        
+        $sql .=  'path like "'.$this->subtree.'%" and name="output-mimetype" and value="httpd/unix-directory"';
+        
+        if (isset($args['order'])) {
+           $sql .= ' order by ' . $args['order'];  
+        } else {
+            $sql .= ' order by path';
+        }
+        
+        $res = $this->db->query($sql);
+        if(MDB2::isError($res)) {
+            return array();
+        }
+        $result = array();
+        $result[0] = "None";
+        while ($row = $res->fetchRow()) {
+
+               $result[$row[0]] = $row[0];
+        }
+        return $result;
+    }
+    
+    
+    
+    
+}
+
+?>
