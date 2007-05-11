1bb627f1efd199ea5b752374eebfe86d6d761c7d
chregu <chregu@99f7f646-bdd5-0310-89f9-ddcb10b10bf3>
2007-05-11 13:47:38 +0000

    replace bitflux with liip, 2006 with 2007 and bitflux.org with flux-cms.org
    git-svn-id: https://svn.liip.ch/repos/public/fluxcms_demo/trunk@8648 99f7f646-bdd5-0310-89f9-ddcb10b10bf3

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 // +----------------------------------------------------------------------+
-// | popoon                                                               |
+// | Flux CMS                                                             |
 // +----------------------------------------------------------------------+
-// | Copyright (c) 2001,2002,2003,2004 Bitflux GmbH                       |
+// | Copyright (c) 2001-2007 Liip AG                                      |
 // +----------------------------------------------------------------------+
 // | This program is free software; you can redistribute it and/or        |
 // | modify it under the terms of the GNU General Public License (GPL)    |
@@ -10,7 +10,7 @@
 // | of the License, or (at your option) any later version.               |
 // | The GPL can be found at http://www.gnu.org/licenses/gpl.html         |
 // +----------------------------------------------------------------------+
-// | Author: Christian Stocker <chregu@bitflux.ch>                        |
+// | Author: Liip AG <contact@liip.ch>                                    |
 // +----------------------------------------------------------------------+
 //
 // $Id$
