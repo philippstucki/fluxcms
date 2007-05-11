4729c52edbf74b78a636bff20d4ad77774d3f482
benjamin <benjamin@99f7f646-bdd5-0310-89f9-ddcb10b10bf3>
2007-05-11 10:03:13 +0000

    latest db version update
    git-svn-id: https://svn.liip.ch/repos/public/fluxcms/trunk@8634 99f7f646-bdd5-0310-89f9-ddcb10b10bf3

--- inc/bx/init.php
+++ inc/bx/init.php
@@ -9,7 +9,7 @@ class bx_init {
     
     static $tmpdir = "./tmp/";
     static $bxdir = "./inc/bx/";
-    static $lastdbversion = 8453;
+    static $lastdbversion = 8633;
     static $configCachedFile = '' ;
     
     
