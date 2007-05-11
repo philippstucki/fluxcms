d19d7fd22ece4349d858ec2ef2f768e503d06a74
chregu <chregu@99f7f646-bdd5-0310-89f9-ddcb10b10bf3>
2007-05-11 13:32:05 +0000

    cleanup and note that it's not really usable
    git-svn-id: https://svn.liip.ch/repos/public/fluxcms/trunk@8640 99f7f646-bdd5-0310-89f9-ddcb10b10bf3

--- inc/bx/dbforms2/liveselect/pagetree.php
+++ inc/bx/dbforms2/liveselect/pagetree.php
@@ -1,5 +1,5 @@
 <?php
-
+/* not really useful right now , but it basically works */
 class bx_dbforms2_liveselect_pagetree extends bx_dbforms2_liveselect {
     
     
@@ -23,37 +23,12 @@ class bx_dbforms2_liveselect_pagetree extends bx_dbforms2_liveselect {
         
         
         if ($q) {
-            $where.= " path like '%$q%' ";
+            $where.= " path like '%$q%' AND ";
         }
         if ($this->where) {
-            $where .=" AND ". $this->replaceTablePrefix($this->where);
+            $where .= $this->replaceTablePrefix($this->where). " AND ";
         }
-        return 'from '.$table .' where '. $where .' AND path like "'.$this->subtree.'%" and name="output-mimetype" and value="httpd/unix-directory"';
-        /*$q = bx_helpers_string::utf2entities(utf8_encode($this->getNormalizedQuery()));
-        
-        $table = $this->tablePrefix.$this->tableName;
-        
-        $whereFields = explode(',', $this->whereFields);
-        $where = '(0 ';
-        foreach($whereFields as $field) {
-            $where.= "OR $field like '%$q%' ";
-        }
-        $where .=" ) ";
-        
-        $notNullFields = explode(',', $this->notNullFields);
-        foreach($notNullFields as $field) {
-			if($field != ''){
-				$where.= "AND $field != 'NULL' ";
-			}
-        }
-        
-        if ($this->where) {
-            $where .=" AND ". $this->replaceTablePrefix($this->where);
-        }
-        
-        $orderby = !empty($this->orderBy) ? $this->replaceTablePrefix($this->orderBy) : $this->idField;
-		$matcher = (!empty($this->getMatcher) AND isset($_GET[$this->getMatcher]) )? ' AND '.$this->getMatcher.' = "'.$_GET[$this->getMatcher].'" ' : '';
-        return 'FROM '.$table.' '. $this->leftJoin .' WHERE '.$where.$matcher.' ORDER BY '.$orderby;*/
-        
+        return 'from '.$table .' where '. $where .'  path like "'.$this->subtree.'%" and name="output-mimetype" and value="httpd/unix-directory"';
+
     }
 }
