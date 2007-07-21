eb405d71effec5f3859c781b5f4328012356826d
chregu <chregu@99f7f646-bdd5-0310-89f9-ddcb10b10bf3>
2007-07-21 20:09:18 +0000

    boxes plugin initial release
    git-svn-id: https://svn.liip.ch/repos/public/fluxcms/trunk@8878 99f7f646-bdd5-0310-89f9-ddcb10b10bf3

---
+++ inc/bx/editors/boxes.php
@@ -0,0 +1,267 @@
+<?php
+
+
+
+class bx_editors_boxes extends bx_editor implements bxIeditor {    
+    
+	private $tablePrefix;
+	private $db;	
+    private $boxesTable = 'boxes';
+    private $boxes2pageTable = 'boxes2page';
+    private $boxesTableScope = 'boxes_scope';
+    private $lang;
+    private $defaultLang;
+    private $langsAvail;
+    private $allScopes;
+    private $scope;
+    
+    private $propertyname = 'set-id';
+    private $namespace = 'box:';
+    
+    private $setId;
+    
+    private $usedBoxes = array();
+    
+    public function __construct(){
+        $this->tablePrefix = $GLOBALS['POOL']->config->getTablePrefix();
+        $this->db = $GLOBALS['POOL']->db; 
+        
+        //some lang stuff
+        $this->defaultLang = $GLOBALS['POOL']->config['defaultLanguage']; 
+        $this->langsAvail = $GLOBALS['POOL']->config['outputLanguages']; 
+        if(isset($_GET['lang']) && in_array($_GET['lang'], $this->langsAvail)){
+            $this->lang = $_GET['lang'];
+        }
+        else{
+            $this->lang = $this->defaultLang;
+        }
+        
+        //and the scope
+        $this->allScopes = $this->getScopes();
+        //print_r($this->allScopes);
+        if(isset($_GET['scope']) && isset($this->allScopes[$_GET['scope']]) ){
+            $this->scope = $_GET['scope'];
+        }
+        else{
+            $this->scope = 0;
+        }
+
+    }
+    public function getDisplayName() {
+        return 'Boxes Editor';
+    }	
+
+    public function getPipelineParametersById($path, $id) { 
+        return array('pipelineName'=>'boxes');
+    }
+
+    
+    public function handlePOST($path, $id, $data) {
+       if(isset($data['boxes']['list'])){
+            foreach($data['boxes']['list'] as $key => $list){
+                // list 0 are the available boxes
+                // no action required
+                if($key == 0){
+                    return;
+                }
+                $this->saveCol($key,$list,$data['boxes']);
+                return;
+            }
+       } 
+    }
+     
+    public function getEditContentById($id) {
+        //check for the boxes set id in the properties table
+        //echo $id;
+
+       
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $dom = new DomDocument();
+            if (strpos($id,"/box-list-update/") !== false) {
+                $dom->appendChild($dom->createElement("ajaxpost"));
+                return $dom;
+            }
+        } 
+
+        $this->setId = $this->checkPropertiesId($id);
+        //echo $id;   
+        $xml  = '<boxes>';
+        $xml .= '<setId>'.$this->setId.'</setId>';
+        $xml .= '<path>'.$id.'</path>';
+        $xml .= $this->langInformation();
+        $xml .= $this->getUsedBoxes(1);
+        $xml .= $this->getUsedBoxes(2);
+        $xml .= $this->getAllBoxes();
+        $xml .= $this->getScopesXML();
+        $xml .= '</boxes>';
+        $dom = new DomDocument();
+        $dom->loadXML($xml);
+        //echo $xml;
+        return $dom;
+        //return $xml;
+    }
+
+
+    public function getMimeType(){
+    
+    }
+    
+    //internal functions -----------------------------------------------------------------------
+    //------------------------------------------------------------------------------------------
+    
+    private function saveCol($col, $list, $data) {
+        print_r($data);
+        print_r($list);
+        
+        $nm = $this->tablePrefix.$this->boxes2pageTable;
+        
+        $lang = $data['lang'];
+        $scope = $data['scope'];
+        $setid = $data['setid'];
+        
+        $query  = " DELETE FROM $nm ";
+        $query .= " WHERE lang = '$lang' AND scope = '$scope' AND col = '$col' AND setid = '$setid'  ";
+
+        $res = $this->db->query($query);
+        if (MDB2::isError($res)) {
+             throw new PopoonDBException($res);
+        }
+        
+        foreach($list as $boxid){
+            $query  = " INSERT INTO  $nm ";
+            $query .= " (lang,scope,col,setid,boxid)  ";
+            $query .= " VALUES ('$lang','$scope','$col','$setid','$boxid')  ";
+
+            $res = $this->db->query($query);
+            if (MDB2::isError($res)) {
+                throw new PopoonDBException($res);
+            }   
+        }
+
+    }
+    
+    /**
+    * searches the id from the properties table
+    * or creates one, if not found
+    */
+    private function checkPropertiesId($path) {
+        $id = bx_resourcemanager::getProperty($path, $this->propertyname, $this->namespace);
+        if(!$id){
+            $id = $this->db->nextID("_sequences"); 
+            bx_resourcemanager::setProperty($path, $this->propertyname, $id, $this->namespace);
+        }
+        return $id;
+    }
+    
+    
+    /**
+    * return language informations
+    */
+    private function langInformation() {
+        $xml = '';
+        foreach($this->langsAvail as $lang){
+            $xml .= '<lang value="'.$lang.'" ';
+            if($lang == $this->defaultLang){
+                $xml .= 'default="true" ';
+            }
+            if($lang == $this->lang){
+                $xml .= 'selected="true" ';
+            }
+            $xml .= '/>';
+        }
+        return $xml;
+    }
+    
+    /**
+    * return language informations
+    */
+    private function getScopesXML() {
+        $xml = '';
+        foreach($this->allScopes as $key => $name){
+            $xml .= '<scope id="'.$key.'" name="'.$name.'" ';
+
+            if($key == $this->scope){
+                $xml .= 'selected="true" ';
+            }
+
+            $xml .= '/>';
+        }
+        return $xml;
+    }
+    
+    /**
+    * returns scope 
+    */
+    private function getScopes() {
+        $query = 'SELECT * from '.$this->tablePrefix.$this->boxesTableScope;
+        //echo $query;
+        $res = $this->db->query($query);
+        if (MDB2::isError($res)) {
+             throw new PopoonDBException($res);
+        } 
+        $arr = array('all' => 'all');
+        while($row = $res->fetchRow(MDB2_FETCHMODE_ASSOC)){
+            $arr[$row['id']] = $row['name'];
+        } 
+        return $arr;
+    }    
+    
+    /**
+    * returns all boxes from the boxes table
+    */
+    private function getAllBoxes() {
+        $query = 'SELECT * from '.$this->tablePrefix.$this->boxesTable.' WHERE lang = "'.$this->lang.'" ';
+        //echo $query;
+        $res = $this->db->query($query);
+        if (MDB2::isError($res)) {
+             throw new PopoonDBException($res);
+        } 
+        $xml = '<allboxes>';  
+        while($row = $res->fetchRow(MDB2_FETCHMODE_ASSOC)){
+            if(in_array($row['id'],$this->usedBoxes)){
+                continue;
+            }
+            $xml .= '<box id="box_'.$row['id'].'">';
+            $xml .= '<title>'.$row['title'].'</title>';
+            $xml .= '<id>'.$row['id'].'</id>';
+            $xml .= '</box>';
+        } 
+        $xml .= '</allboxes>';
+        return $xml;
+    }
+    
+    /**
+    * returns all used  boxes in this page
+    */
+    private function getUsedBoxes($col = 0){
+        $nm = $this->tablePrefix.$this->boxes2pageTable;
+        $box = $this->tablePrefix.$this->boxesTable;
+
+        $query  = " SELECT $box.* FROM $nm ";
+        $query .= " JOIN $box ON ( $nm.boxid  = $box.id) ";
+        $query .= ' WHERE '.$nm.'.lang = "'.$this->lang.'" AND '.$nm.'.scope = "'.$this->scope.'" AND '.$nm.'.col = "'.$col.'" ';
+        $query .= " AND $nm.setid = ".$this->setId." ";
+        $query .= " ORDER BY $nm.rang ";
+
+        $res = $this->db->query($query);
+        if (MDB2::isError($res)) {
+             throw new PopoonDBException($res);
+        } 
+        $xml = "<box_$col>";  
+        while($row = $res->fetchRow(MDB2_FETCHMODE_ASSOC)){
+            $this->usedBoxes[] = $row['id'];
+            $xml .= '<box id="box_'.$row['id'].'">';
+            $xml .= '<title>'.$row['title'].'</title>';
+            $xml .= '<id>'.$row['id'].'</id>';
+            $xml .= '</box>';
+        } 
+        $xml .= "</box_$col>";
+        return $xml;
+    }
+    
+    
+   
+    
+}
+
+?>
---
+++ inc/bx/plugins/boxes.php
@@ -0,0 +1,293 @@
+<?php
+
+// +----------------------------------------------------------------------+
+// | Bx                                                                   |
+// +----------------------------------------------------------------------+
+// | Copyright (c) 2005 Liip AG                                           |
+// +----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or        |
+// | modify it under the terms of the GNU General Public License (GPL)    |
+// | as published by the Free Software Foundation; either version 2       |
+// | of the License, or (at your option) any later version.               |
+// | The GPL can be found at http://www.gnu.org/licenses/gpl.html         |
+// +----------------------------------------------------------------------+
+// | Author: Liip AG      <devel@liip.ch>                              |
+// +----------------------------------------------------------------------+
+
+/** 
+* To use this plugin in a collection, put the following into .configxml
+*** 
+<bxcms xmlns="http://bitflux.org/config">
+<plugins>
+<parameter name="xslt" type="pipeline" value="boxes.xsl"/>
+<extension type="html"/>
+<plugin type="boxes" >
+</plugin>
+<plugin type="navitree"></plugin>
+</plugins>
+</bxcms>
+***
+* See also the boxes.xsl in your themes folder for the actual output
+*/
+
+class bx_plugins_boxes extends bx_plugin implements bxIplugin {
+    
+    /**
+    * a static var to to save the instances of this plugin
+    */
+    static public $instance = array();
+    protected $res = array();
+    public $name = "boxes";
+    
+    /**
+    The table names
+    */
+    public $tablename = null;
+    
+    protected $db = null;
+    protected $tablePrefix = null;
+    
+    /**
+    * plugins are singleton, they only exists once (for different modes)
+    *  per request. The $mode stuff isn't really used, but may be in 
+    *  future releases.
+    */
+    public static function getInstance($mode) {
+        
+        if (!isset(self::$instance[$mode])) {
+            self::$instance[$mode] = new bx_plugins_boxes($mode);
+        } 
+        return self::$instance[$mode];
+    }
+    
+    /** 
+    * You are not allowed to call the constructor from outside, therefore
+    *  it's protected. You have to use getInstance()
+    */
+    protected function __construct($mode) {
+        // Get the global table prefix
+        $this->tablePrefix = $GLOBALS['POOL']->config->getTablePrefix();
+        // get the db object
+        $this->db = $GLOBALS['POOL']->db;
+        $this->mode = $mode;
+    }
+    
+    /*** 
+    Action methods. 
+    This are called from the bxcms popoon action 
+    ***/
+    
+    /**
+    * This function is called by the action to check, if it's a "RealResource"
+    *  meaning that it actually has something to display
+    * If all plugins in a collection return false, a page not found exception is 
+    *  thrown
+    * For this plugin, we just assume, it has always "something to say"
+    *
+    * @param    string  $path   The collectionURI
+    * @param    string  $id     The id of the request, 
+    *                           returned by getIdByRequest                          
+    * @return   bool            If it is a RealResource or not.
+    * @see      getIdByRequest 
+    */
+    public function isRealResource($path , $id) {
+        return true;
+    }
+    
+    /**
+    * Every plugin has to return a unique id for a request.
+    * If we for example are in the collection /links/
+    *  and the request is /links/foobar.html, we get
+    *  $path = /links/, $name="foobar", $ext="html"
+    * If the request is /links/something/foobar.html
+    *  and there is no collection "something", then name
+    *  is "something/foobar"
+    * Usually you should not be too concerned about the extension
+    *  since that can be differently, if you do match on different
+    *  extensions in .configxml
+    *
+    * In this example, we just return the filename part and add
+    *  .links to it, to make it unique
+    *
+    * @param    string  $path   The collectionURI
+    * @param    string  $name   The filename part of the request
+    * @param    string  $ext    The extension part of the request
+    * @return   string          A unique id
+    */
+    
+    public function getIdByRequest($path, $name = NULL, $ext = NULL) {
+        
+        return $this->name;
+        
+    } 
+    
+    /***
+    Content methods
+    The actual content getting methods
+    ***/
+    
+    /**
+    * The actual "pulling the content" method
+    * This gets called in the bxcms generator popoon component
+    * It has to return a DomDocument with the content
+    *
+    * @param    string  $path   The collectionURI
+    * @param    string  $id     The id of the request, 
+    *                           returned by getIdByRequest                          
+    * @return   DomDocument     A DomDocument with the content
+    * @see      getIdByRequest 
+    */
+    
+    public function getContentById($path, $id) {
+        
+        $this->tablename = $this->getParameter($path,"tablename");
+        if ($this->tablename) {
+            // show all links if id = index.links
+            if ($id == "index.boxes") {
+                $xml =  $this->getData($path);
+                
+            } else {
+                //otherwise show only a single link
+                // casting to (int) will get rid of .links
+                $xml = $this->getData($path, " id = ". (int) substr($id,2));
+            }
+        } else {
+            $xml = $this->getEmptyPage($id);
+        }
+        
+        $xml = '<div xmlns="http://www.w3.org/1999/xhtml"  xmlns:i18n="http://apache.org/cocoon/i18n/2.1">'.$xml.'</div>';
+        
+        return domdocument::loadXML($xml);
+    }
+    
+    /***
+    UNTIL HERE IS ALL WHAT IT NEEDS FOR A BASIC IMPLEMENTATION
+    (to just output a page with all links)
+    What follows is additional juice.
+    ***/
+    
+    public function _getOverviewSections($path) {
+        
+    echo $path;
+    
+        $sections = array();
+        $dom = new bx_domdocs_overview();
+        
+        $dom->setTitle("Boxes");
+        $dom->setPath($path);
+        $dom->setType("boxes");
+        $dom->setIcon("gallery");
+        
+        // first tab
+        $dom->addLink("Create new box",'dbforms2/boxes/');
+           // $dom->addLink("Create Newsletter",'addresource'.$path.'drafts/?type=xhtml');
+           // $dom->addLink("Send Newsletter",'edit'.$path.'send/');
+        
+        // second tab
+       // $dom->addTab("Creation");
+            //$dom->addLink("User Management",'edit'.$path.'users/');
+        
+        return $dom;
+    }
+    
+    public function getResourceById($path, $id, $mock = false) {
+        /*$pathid = $path.$id;
+        if (!isset($this->res[$pathid])) {
+            $res = new bx_resources_simple($pathid);
+            $id = (int) $id;
+            $res->props['title'] = $this->db->queryOne("select text from ".$this->tablePrefix.$this->linksTable." where id = ".$id); 
+            $res->props['outputUri'] = $path.$id.".html"; 
+            $res->props['resourceDescription'] = "Link";
+            $this->res[$pathid] = $res;
+        }
+        return $this->res[$pathid];
+        */
+    }
+    
+    /***
+    admin methods
+    ***/  
+    
+    /**
+    * to actually being able to edit links in the admin, we have to return
+    *  true here, if the admin actions asks us for that.
+    * We don't care about path,id, etc here
+    */
+    
+    public function adminResourceExists($path, $id, $ext=null, $sample = false) {
+        if($ext == 'xhtml'){
+            return false;
+        }
+        return true;
+    }
+    /**
+    * we need to "register" what editors are beeing able to handle this plugin
+    */
+    
+    public function getEditorsById($path, $id) {
+        return array("boxes");
+        //return null;
+    }
+    
+    /***
+    Internal Methods, only needed by that class
+    ***/
+    
+    
+    protected function getEmptyPage($id) {
+        
+        $xml = '<p>
+        <i18n:text>Called boxes plugin with id: '.$id.'</i18n:text></p>';
+        
+        return $xml;
+    }
+    
+    /**
+    * Returns all links as XML
+    */
+    
+    protected function getData($path, $where = null) {
+        $query = "select * from ".$this->tablePrefix.$this->tablename;
+        
+        if ($where) {
+            $query .= " where $where ";
+        }
+        
+        
+        $res = $this->db->query($query);
+        
+        $xml = "<table>";
+        $row = $res->fetchRow(MDB2_FETCHMODE_ASSOC);
+        $overview = ($res->numRows() > 1) ? true : false;
+        
+        if ($overview) {
+            array_splice($row,4);
+        }
+        
+        $xml .= "<tr>";
+        foreach ($row as $key => $value) {
+                $xml .="<th>".htmlspecialchars($key)."</th>";   
+            }
+        $xml .= "</tr>";
+        
+        do {
+            $xml .="<tr>";
+            if ($overview) {
+                array_splice($row,4);
+            }
+            foreach ($row as $key => $value) {
+                $xml .="<td>".$value."</td>";   
+            }
+            $xml .="</tr>";
+        } while ($row = $res->fetchRow(MDB2_FETCHMODE_ASSOC));
+        
+        $xml .= "</table>";
+        return $xml;            
+        
+    }
+    
+    public function getMimeType(){
+    
+    }
+    
+  }
