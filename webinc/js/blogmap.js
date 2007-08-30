06a515c4827e2e37fe4355c0d9d3f76d0de45757
benjamin <benjamin@99f7f646-bdd5-0310-89f9-ddcb10b10bf3>
2007-08-30 15:09:08 +0000

    blog_map now centers and makes bigger bables
    git-svn-id: https://svn.liip.ch/repos/public/fluxcms/trunk@8968 99f7f646-bdd5-0310-89f9-ddcb10b10bf3

--- webinc/js/blogmap.js
+++ webinc/js/blogmap.js
@@ -1,9 +1,12 @@
-function createMarker(point, number, title, link, image) {
+function createMarker(point, number, title, link, image, content, author, date) {
+    console.log(title);
   var marker = new GMarker(point);
   GEvent.addListener(marker, "click", function() {
     marker.openInfoWindowHtml(
-        "<b><a href='"+ link +"'>" + title + "</a></b><br/><img src='"+ image +"' alt='"+ image +"' width='100'/>"
-    );
+        "<div style=\"height: 200px; width:400px; padding-bottom:20px;\"><b><a href='"+ link +"'>" + title + "</a></b>"+
+        "<br/>"+ author +" @ "+ date +
+        "<br/><img src='"+ image +"' alt='"+ image +"' width='100'/><br/>"+ content +"</div>"
+     );
   });
   return marker;
 }
@@ -11,6 +14,7 @@ function createMarker(point, number, title, link, image) {
 function load() {
     if (GBrowserIsCompatible()) {
         var map = new GMap2(document.getElementById("map"));
+        var bounds = new GLatLngBounds();
         map.addControl(new GSmallMapControl());
         map.addControl(new GMapTypeControl());
         
@@ -18,14 +22,19 @@ function load() {
         for (var i = 0; i < locations.length; ++i) {
             if(locations[i] != undefined) {
                 if(i == 1) {
-                    map.setCenter(new GLatLng(locations[i]['lat'], locations[i]['lon']), 6);
+                    map.setCenter(new GLatLng(locations[i]['lat'], locations[i]['lon']));
                 }
                 var point = new GLatLng(locations[i]['lat'], locations[i]['lon']);
                 
-                map.addOverlay(createMarker(point, i + 1, locations[i]['title'], locations[i]['link'], locations[i]['image']));
-
+                map.addOverlay(createMarker(point, i + 1, locations[i]['title'], locations[i]['link'], locations[i]['image'], locations[i]['content'], locations[i]['author'], locations[i]['date']));
+                bounds.extend(point);
             }
         }
+        map.setZoom(map.getBoundsZoomLevel(bounds));
+        var clat = (bounds.getNorthEast().lat() + bounds.getSouthWest().lat()) /2;
+        var clng = (bounds.getNorthEast().lng() + bounds.getSouthWest().lng()) /2;
+        map.setCenter(new GLatLng(clat,clng));
+
         //map.setCenter(new GLatLng(37.4419, -122.1419), 13);
     }
 }
