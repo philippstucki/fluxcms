43b98a47b5031d3ae889008713490a83d2c0e7d8
chregu <chregu@99f7f646-bdd5-0310-89f9-ddcb10b10bf3>
2007-09-29 03:00:34 +0000

    automatic BuildDate Update
    git-svn-id: https://svn.liip.ch/repos/public/fluxcms/trunk@9092 99f7f646-bdd5-0310-89f9-ddcb10b10bf3

--- inc/bx/init.php
+++ inc/bx/init.php
@@ -1,7 +1,7 @@
 <?php
 
 define('BXCMS_VERSION', "1.6-dev");
-define('BXCMS_BUILD','200709250500');
+define('BXCMS_BUILD','200709290500');
 //define('BXCMS_REVISION',preg_replace('/\$Rev$'));    
 define('BXCMS_REVISION', '$Rev$');
 
