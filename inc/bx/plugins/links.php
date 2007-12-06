658d43cbb1fc2e7ffb0da23a8653702cf1ee3bfa
gassi <gassi@99f7f646-bdd5-0310-89f9-ddcb10b10bf3>
2007-12-06 17:27:03 +0000

    error fix
    git-svn-id: https://svn.liip.ch/repos/public/fluxcms/trunk@9324 99f7f646-bdd5-0310-89f9-ddcb10b10bf3

--- inc/bx/plugins/links.php
+++ inc/bx/plugins/links.php
@@ -14,13 +14,13 @@
 // | Author: Liip AG      <devel@liip.ch>                              |
 // +----------------------------------------------------------------------+
 
-/** 
+/**
 * This plugin shows a list of links from the bloglinks table
 * It shows them in categories
 * It will also use some metaindex features for tags and searching
 *
 * To use this plugin in a collection, put the following into .configxml
-*** 
+***
     <bxcms xmlns="http://bitflux.org/config">
         <plugins>
             <parameter name="xslt" type="pipeline" value="links.xsl"/>
@@ -37,36 +37,36 @@
 */
 
 class bx_plugins_links extends bx_plugin implements bxIplugin {
-    
+
     /**
     * a static var to to save the instances of this plugin
     */
     static public $instance = array();
     protected $res = array();
-    
+
     /**
     The table names
     */
     public $linksTable = "bloglinks";
     public $categoryTable = "bloglinkscategories";
-    
+
     protected $db = null;
     protected $tablePrefix = null;
-    
+
     /**
     * plugins are singleton, they only exists once (for different modes)
-    *  per request. The $mode stuff isn't really used, but may be in 
+    *  per request. The $mode stuff isn't really used, but may be in
     *  future releases.
     */
     public static function getInstance($mode) {
-        
+
         if (!isset(self::$instance[$mode])) {
             self::$instance[$mode] = new bx_plugins_links($mode);
-        } 
+        }
         return self::$instance[$mode];
     }
-    
-    /** 
+
+    /**
     * You are not allowed to call the constructor from outside, therefore
     *  it's protected. You have to use getInstance()
     */
@@ -77,29 +77,29 @@ class bx_plugins_links extends bx_plugin implements bxIplugin {
         $this->db = $GLOBALS['POOL']->db;
          $this->mode = $mode;
     }
-    
-    /*** 
-        Action methods. 
-        This are called from the bxcms popoon action 
+
+    /***
+        Action methods.
+        This are called from the bxcms popoon action
      ***/
-    
+
     /**
     * This function is called by the action to check, if it's a "RealResource"
     *  meaning that it actually has something to display
-    * If all plugins in a collection return false, a page not found exception is 
+    * If all plugins in a collection return false, a page not found exception is
     *  thrown
     * For this plugin, we just assume, it has always "something to say"
     *
     * @param    string  $path   The collectionURI
-    * @param    string  $id     The id of the request, 
-    *                           returned by getIdByRequest                          
+    * @param    string  $id     The id of the request,
+    *                           returned by getIdByRequest
     * @return   bool            If it is a RealResource or not.
-    * @see      getIdByRequest 
+    * @see      getIdByRequest
     */
     public function isRealResource($path , $id) {
         return true;
     }
-    
+
     /**
     * Every plugin has to return a unique id for a request.
     * If we for example are in the collection /links/
@@ -120,36 +120,36 @@ class bx_plugins_links extends bx_plugin implements bxIplugin {
     * @param    string  $ext    The extension part of the request
     * @return   string          A unique id
     */
-    
+
     public function getIdByRequest($path, $name = NULL, $ext = NULL) {
-       
+
        return $name.'.'.$this->name;
-       
-    } 
-    
+
+    }
+
     /***
         Content methods
         The actual content getting methods
      ***/
-    
+
     /**
     * The actual "pulling the content" method
     * This gets called in the bxcms generator popoon component
     * It has to return a DomDocument with the content
     *
     * @param    string  $path   The collectionURI
-    * @param    string  $id     The id of the request, 
-    *                           returned by getIdByRequest                          
+    * @param    string  $id     The id of the request,
+    *                           returned by getIdByRequest
     * @return   DomDocument     A DomDocument with the content
-    * @see      getIdByRequest 
+    * @see      getIdByRequest
     */
-    
+
     public function getContentById($path, $id) {
-        
+
         // get the dirname and check, if we have one
         // this helps finding links like category/3.html
         $dirname = dirname($id);
-        
+
         switch ($dirname) {
             case "category":
                 //get the cat id out of $id
@@ -176,86 +176,87 @@ class bx_plugins_links extends bx_plugin implements bxIplugin {
                     return $this->getLinks($path, " links.id = ". (int) $id);
                 }
         }
-        
-        
+
+
     }
-    
+
     /***
        UNTIL HERE IS ALL WHAT IT NEEDS FOR A BASIC IMPLEMENTATION
        (to just output a page with all links)
         What follows is additional juice.
      ***/
-    
-    
+
+
     public function getResourceById($path, $id, $mock = false) {
         $pathid = $path.$id;
         if (!isset($this->res[$pathid])) {
             $res = new bx_resources_simple($pathid);
             $id = (int) $id;
-            $res->props['title'] = $this->db->queryOne("select text from ".$this->tablePrefix.$this->linksTable." where id = ".$id); 
+            $res->props['title'] = $this->db->queryOne("select text from ".$this->tablePrefix.$this->linksTable." where id = ".$id);
 
-		if(MDB2::isError($res->props['title']){throw new PopoonDBException($res);}
-            $res->props['outputUri'] = $path.$id.".html"; 
+        if(MDB2::isError($res->props['title'] )){
+            throw new PopoonDBException($res);}
+            $res->props['outputUri'] = $path.$id.".html";
             $res->props['resourceDescription'] = "Link";
             $this->res[$pathid] = $res;
         }
         return $this->res[$pathid];
     }
-    
+
     /***
        admin methods
-     ***/  
-     
+     ***/
+
     /**
     * to actually being able to edit links in the admin, we have to return
     *  true here, if the admin actions asks us for that.
     * We don't care about path,id, etc here
     */
-    
+
     public function adminResourceExists($path, $id, $ext=null, $sample = false) {
         return true;
     }
     /**
     * we need to "register" what editors are beeing able to handle this plugin
     */
-    
+
     public function getEditorsById($path, $id) {
         return array("links");
     }
-    
+
     /***
         Internal Methods, only needed by that class
     ***/
-    
+
     /**
     * Returns all links as XML
     */
-    
+
     protected function getLinks($path, $where = null) {
-       
-        
+
+
         /* for the sake of simplicity, we use XML_db2xml,
             which makes it quite easy to get an XML out of a DB query
             even, if it's not that nice to look at
             */
         $db2xml = new XML_db2xml($this->db,"links");
          $style = $this->getParameter($path,"style");
-         
+
         if ($style == "delicious") {
          $query = "select links.link, links.text, links.id, links.description, links.date from ".$this->tablePrefix.$this->linksTable." as links ";
-                    
+
         } else {
-        
+
         $query = "select categories.id, categories.name, links.id, links.text, links.link, links.description, links.date from ".$this->tablePrefix.$this->categoryTable." as categories
                     left join ".$this->tablePrefix.$this->linksTable." as links
-                    on links.bloglinkscategories  = 
+                    on links.bloglinkscategories  =
                        categories.id";
         }
         if ($where) {
             $query .= " where $where ";
         }
-        
-       
+
+
         if ($style == "delicious") {
               $query .= " order by links.date DESC";
               $idxpath = "/links/result/row/id";
@@ -264,13 +265,13 @@ class bx_plugins_links extends bx_plugin implements bxIplugin {
             $idxpath = "/links/result/row/row/id";
         }
         $res = $this->db->query($query);
-       if(MDB2::isError($res){
+       if(MDB2::isError($res) ){
              throw new PopoonDBException($res);
        }
- 
+
         //get ids from the 3rd col for the later tags retrieval
         $_ids = $res->fetchCol(2);
-        
+
         //seek back to the start of the result set
         $res->seek(0);
         //and make a domdocument out of that resultset
@@ -283,7 +284,7 @@ class bx_plugins_links extends bx_plugin implements bxIplugin {
         }
         // get tags from metaindex with the found linkids from above
         $tags = bx_metaindex::getTagsByIds($linksids);
-        
+
         //create a xpath object
         $xp = new domxpath($dom);
         //loop through all link ids
@@ -308,6 +309,6 @@ class bx_plugins_links extends bx_plugin implements bxIplugin {
         }
         //add the style to the document element for using it in the xslt
         $dom->documentElement->setAttribute("style",$style);
-        return $dom;           
+        return $dom;
     }
 }
