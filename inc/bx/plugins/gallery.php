ba21e98033c4dbf606edda2d8abf7c23b5eef410
brian <brian@99f7f646-bdd5-0310-89f9-ddcb10b10bf3>
2008-05-13 14:21:34 +0000

    by manuel: lightbox plugin
    git-svn-id: https://svn.liip.ch/repos/public/fluxcms/trunk@10366 99f7f646-bdd5-0310-89f9-ddcb10b10bf3

--- inc/bx/plugins/gallery.php
+++ inc/bx/plugins/gallery.php
@@ -179,7 +179,7 @@ class bx_plugins_gallery extends bx_plugin {
         
         // get needed parameters from .config.xml file
         $params = $dom->importNode($this->getXMLNodeByParameters($path, array('rowsPerPage', 'columnsPerPage')), TRUE);
-       
+
         
         $albumTree = $dom->importNode($this->getAlbumTree($this->getGalleryRoot($path), $relPath), TRUE);
 
@@ -188,7 +188,11 @@ class bx_plugins_gallery extends bx_plugin {
         $dom->documentElement->appendChild($params);
         $dom->documentElement->appendChild($pagerNode);
         $dom->documentElement->appendChild($albumTree);
-
+	
+	
+	$element = $dom->createElement('lightBoxOn', $this->getParameter($path, "lightBoxOn"));
+	$dom->documentElement->appendChild($element);
+	
         return $dom;
         
     }
@@ -214,6 +218,7 @@ class bx_plugins_gallery extends bx_plugin {
     
     protected function getAlbumTree($root, $path) {
         $dom = new DomDocument();
+
         $treeNode = $dom->createElement('albumTree');
         
         $dirs = array('.');
@@ -431,6 +436,171 @@ class bx_plugins_gallery extends bx_plugin {
     }
 }
 
+class bx_plugins_gallery_flickr {
+    
+    protected $photoID = null;
+    
+    static function getInstance($dom,$path,$id,$params) {
+          if (strpos($id,"flickr") === 0) {
+           
+                return new bx_plugins_gallery_flickr($dom,$path,$id,$params);
+          }
+    }
+    
+    function __construct ($dom,$path,$id,$params) {
+            $this->flickrUsername = $params['username'];
+          
+            $flickrID = preg_match("#flickr([0-9]+)/#",$id,$matches);
+            $flickrID = $matches[1];
+            $this->albumID = $matches[1];
+            $photoID = preg_match("#".$matches[1]."/([0-9]+)\.jpg#",$id,$matches);
+            if (isset($matches[1])) {
+                $this->photoID = $matches[1];
+            }
+            $this->f = new Services_flickr($params['userid']);
+            $this->dom = $dom;
+    }
+    
+    function getImagesAndAlbums (&$options) {
+		$photos = $this->f->getPhotos($this->albumID);
+        foreach ($photos as $set) {
+            
+            
+            
+            if(($options['mode']=='image') 
+                || (($options['numberOfImages'] + 1 > ($options['currentPage'] - 1) * $options['imagesPerPage']) 
+                    && ($options['numberOfImages'] + 1<= ($options['currentPage']) * $options['imagesPerPage']))) {
+                $url = $this->f->getPhotoLink($set['id'], $set['secret'], "s");
+                $node = $this->dom->createElement('image');
+                $node->setAttribute('href', $set['id'].".jpg");
+                $node->setAttribute('imgsrc', $url);
+                $node->setAttribute('id', $set['id']);
+                $options['images']->appendChild($node);
+                if ($set['id'] == $this->photoID) {
+                    $link = $this->f->getPhotoLink($this->photoID, $set['secret']);
+                    $this->dom->documentElement->setAttribute('imageHref', $link);
+                    $this->dom->documentElement->setAttribute('imageLink', 'http://www.flickr.com/photos/'.$this->flickrUsername.'/'.$this->photoID);
+                    $this->dom->documentElement->setAttribute('imageId',  $this->photoID);
+                }
+            }
+            $options['numberOfImages']++;
+        }
+    }
+    
+    
+    
+}
+
+class bx_plugins_gallery_file {
+	
+    static function getInstance($dom,$path,$id) {
+            return new bx_plugins_gallery_file($dom,$path,$id);
+    }
+    
+    function __construct ($dom,$path, $id) {
+        $this->root = $id;
+        $this->dom = $dom;
+        
+    }
+    
+    function getImagesAndAlbums(& $options) {
+	   $dir = new ImageDirectoryIterator($options['root']);
+	   $files = array();
+        foreach ($dir as $file) {
+            $f = new StdClass(); 
+            $f->name = $file->getFileName();
+            $f->isDot = $file->isDot();
+            $f->isDir = $file->isDir();
+            $f->isReadable = $file->isReadable();
+            $f->isImage = $file->isImage();
+            $files[$f->name] = $f;
+        }
+        ksort($files);
+        foreach ($files as $file) {
+            $name = $file->name;
+            $lang = $GLOBALS['POOL']->config->getOutputLanguage();
+            if(!$file->isDot && $file->isReadable && substr($name,0,1) !== ".") {
+                if ($file->isDir) {
+                    $node = $this->dom->createElement('album');
+                    $node->setAttribute('name', $name);
+                    $node->setAttribute('href', $name.'/');
+                    $prefix = $GLOBALS['POOL']->config->getTablePrefix();
+                    $subgallery = "/".$options['path'].$name."/";
+                    
+                    $query = "select path from ".$prefix."properties where path like '".$subgallery."%' and name = 'preview' and value = '1'";
+                    foreach ( $GLOBALS['POOL']->db->queryCol($query) as $pic) {
+                        $pic = str_replace($subgallery,"",$pic);
+                        
+                        if (strpos($pic,"/") === false) {
+                            $node->setAttribute("preview",$pic);
+                        }
+                    }
+                    $options['albums']->appendChild($node);
+                    
+                    
+                    $options['numberOfAlbums']++;
+                } else if ($file->isImage) {
+                    if(($options['mode']=='image' ) || ($options['numberOfImages'] + 1 > ($options['currentPage'] - 1) * $options['imagesPerPage']) && ($options['numberOfImages'] + 1<= ($options['currentPage']) * $options['imagesPerPage'])) {
+                        $node = $this->dom->createElement('image');
+                        $node->setAttribute('href', $name);
+                        $node->setAttribute('id', $name);
+                        $options['images']->appendChild($node);
+                        //bx_helpers_debug::webdump($options['path'].$name);
+                        /* this code would allow captions and title in overviews as well... */
+                        $preview = bx_resourcemanager::getProperty("/".$options['path'].$name,"preview",'bx:'.$lang);
+                        //bx_helpers_debug::webdump($preview);
+                        if ($preview) {
+                            $node->appendChild($this->dom->createTextNode(html_entity_decode($preview,ENT_COMPAT,"UTF-8")));
+                        }
+                            
+                        if ($options['mode'] != 'image' && $options['descriptionInOverview']) {
+                            
+                            $description = bx_resourcemanager::getProperty("/".$options['path']."subgallery","description",'bx:'.$lang);
+                            if ($description) {
+                                $node->appendChild($this->dom->createTextNode(html_entity_decode($description,ENT_COMPAT,"UTF-8")));
+                            }
+                        }
+                        
+                        if ($options['mode'] != 'image' && $options['titleInOverview']) {
+                            $title = bx_resourcemanager::getProperty("/".$options['path'].$name,"title",'bx:'.$lang);
+                            if ($title) {
+                                $node->setAttribute('imageTitle', html_entity_decode($title,ENT_COMPAT,"UTF-8"));
+                            }
+                        }
+                        
+                        
+                        if ($name == $options['basename']) {
+                            $title = bx_resourcemanager::getProperty("/".$options['path'].$name,"title",'bx:'.$lang);
+                            $description = bx_resourcemanager::getProperty("/".$options['path'].$name,"description",'bx:'.$lang);
+                            
+                            if ($title) {
+                                $title = html_entity_decode($title,ENT_COMPAT,"UTF-8");
+                                $this->dom->documentElement->setAttribute('imageTitle', $title);
+                            
+                            }
+                            if ($description) {
+                                $description = html_entity_decode($description,ENT_COMPAT,"UTF-8");
+                                $this->dom->documentElement->setAttribute('imageDescription', $description);
+                            
+                            }
+                            $this->dom->documentElement->setAttribute('imageHref', $name);
+                            $this->dom->documentElement->setAttribute('imageId', $name);
+                            
+                           $options['numberOfCurrentImage'] = $options['numberOfImages'] + 1 ;
+                           
+                           $this->dom->documentElement->setAttribute('numberOfCurrentImage', $options['numberOfCurrentImage']);
+                           
+                        }
+                        
+                    }
+                    $options['numberOfImages']++;
+                }
+            }
+        }
+       return true;
+    }
+    
+}
 
 
-
+?>
