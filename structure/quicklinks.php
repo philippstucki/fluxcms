3e925e24668f2079201c05d4d7cc701203d7d82d
silvan <silvan@99f7f646-bdd5-0310-89f9-ddcb10b10bf3>
2008-06-29 21:51:36 +0000

    - .. make use of that neat dbforms2 users interface from the quicklinks
    git-svn-id: https://svn.liip.ch/repos/public/fluxcms_demo/trunk@10509 99f7f646-bdd5-0310-89f9-ddcb10b10bf3

--- structure/quicklinks.php
+++ structure/quicklinks.php
@@ -3,7 +3,7 @@
 $quicklinks = array (
 'admin/siteoptions/' => $i18n->translate('Site-Options'),
 // 'admin/dbforms2/blogposts/' => 'Blogposts',
-'users' => 'Users',
+'admin/dbforms2/users' => 'Users',
 'admin/dbforms2/blogpodcasts' => 'Blog - Podcasts',
 'admin/dbforms2/projekte' => 'Projekte',
 'admin/dbforms2/subprojekte' => 'Sub-Projekte',
