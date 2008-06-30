ae8a0dc25035592d99d52828236b5802b159eb68
benjamin <benjamin@99f7f646-bdd5-0310-89f9-ddcb10b10bf3>
2008-06-30 14:00:59 +0000

    removed useless quicklinks
    git-svn-id: https://svn.liip.ch/repos/public/fluxcms_demo/trunk@10513 99f7f646-bdd5-0310-89f9-ddcb10b10bf3

--- structure/quicklinks.php
+++ structure/quicklinks.php
@@ -4,9 +4,6 @@ $quicklinks = array (
 'admin/siteoptions/' => $i18n->translate('Site-Options'),
 // 'admin/dbforms2/blogposts/' => 'Blogposts',
 'admin/dbforms2/users' => 'Users',
-'admin/dbforms2/blogpodcasts' => 'Blog - Podcasts',
-'admin/dbforms2/projekte' => 'Projekte',
-'admin/dbforms2/subprojekte' => 'Sub-Projekte',
 //'options' => 'options (dbform)',
 );
 
