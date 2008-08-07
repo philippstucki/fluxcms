c655bd76e9e685255770203190988fb75cb0edbc
chregu <chregu@99f7f646-bdd5-0310-89f9-ddcb10b10bf3>
2008-08-07 07:38:58 +0000

    test
    git-svn-id: https://svn.liip.ch/repos/public/fluxcms/trunk@10602 99f7f646-bdd5-0310-89f9-ddcb10b10bf3

---
+++ structure/quicklinks.php
@@ -0,0 +1,11 @@
+<?php
+
+$quicklinks = array (
+'admin/siteoptions/' => $i18n->translate('Site-Options'),
+// 'admin/dbforms2/blogposts/' => 'Blogposts',
+'users' => 'Users',
+'admin/dbforms2/right' => 'Rechte Spalte',
+//'options' => 'options (dbform)',
+);
+
+?>
