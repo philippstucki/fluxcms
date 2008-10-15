5cd9c4341bdfc03b5679770ff78bd94cf13f7b5c
chregu <chregu@99f7f646-bdd5-0310-89f9-ddcb10b10bf3>
2008-10-15 12:02:15 +0000

    WS only
    git-svn-id: https://svn.liip.ch/repos/public/popoon/trunk@10865 99f7f646-bdd5-0310-89f9-ddcb10b10bf3

--- components/matchers/uri.php
+++ components/matchers/uri.php
@@ -2,7 +2,7 @@
 // +----------------------------------------------------------------------+
 // | popoon                                                               |
 // +----------------------------------------------------------------------+
-// | Copyright (c) 2001-2006 Bitflux GmbH                                 |
+// | Copyright (c) 2001-2008 Liip AG                                      |
 // +----------------------------------------------------------------------+
 // | Licensed under the Apache License, Version 2.0 (the "License");      |
 // | you may not use this file except in compliance with the License.     |
@@ -14,34 +14,30 @@
 // | implied. See the License for the specific language governing         |
 // | permissions and limitations under the License.                       |
 // +----------------------------------------------------------------------+
-// | Author: Christian Stocker <chregu@bitflux.ch>                        |
+// | Author: Christian Stocker <chregu@liip.ch>                           |
 // +----------------------------------------------------------------------+
 //
 // $Id$
 
 
-
 /**
-* Matches an  uri 
-*
-* @author   Christian Stocker <chregu@bitflux.ch>
-* @version  $Id$
-* @package  popoon
-*/
-class popoon_components_matchers_uri extends popoon_components_matcher
-{
+ * Matches an  uri
+ *
+ * @author   Christian Stocker <chregu@liip.ch>
+ * @version  $Id$
+ * @package  popoon
+ */
+class popoon_components_matchers_uri extends popoon_components_matcher {
 
     function __construct(&$sitemap) {
-		parent::__construct($sitemap);
+        parent::__construct($sitemap);
     }
 
-	function match($value)
-    {
-		return $this->_match($value,$this->sitemap->uri);
-	}
-	
-	function getVarName()
-	{
-		return '$_SERVER["REQUEST_URI"]';
-	}
+    function match($value) {
+        return $this->_match($value, $this->sitemap->uri);
+    }
+
+    function getVarName() {
+        return '$_SERVER["REQUEST_URI"]';
+    }
 }
