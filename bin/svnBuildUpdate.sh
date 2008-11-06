f7b06b2987141e78da0456d9ffa8c5e93fd7b1ce
liipbot <liipbot@99f7f646-bdd5-0310-89f9-ddcb10b10bf3>
2008-11-06 09:19:26 +0000

    use liipbot
    git-svn-id: https://svn.liip.ch/repos/public/fluxcms_demo/trunk@10955 99f7f646-bdd5-0310-89f9-ddcb10b10bf3

--- bin/svnBuildUpdate.sh
+++ bin/svnBuildUpdate.sh
@@ -13,7 +13,7 @@ if test $NEW != $OLD
 then
 date=`date +%-y.%-m.%-d`; sed -e "s/BXCMS_BUILD_DATE','.*/BXCMS_BUILD_DATE','$date');/" inc/bx/init.php > inc/bx/init.php.new ; mv inc/bx/init.php.new inc/bx/init.php
 date=`date +%-H.%-M`; sed -e "s/BXCMS_BUILD_HOUR','.*/BXCMS_BUILD_HOUR','$date');/" inc/bx/init.php > inc/bx/init.php.new ; mv inc/bx/init.php.new inc/bx/init.php
-svn ci -m"automatic BuildDate Update" inc/bx/init.php
+svn ci --username=liipbot  -m"automatic BuildDate Update" inc/bx/init.php
 rm div/phpfiles
 for i in `find $dir -name "*.php"` ; do md5sum $i >> div/phpfiles; done 
 fi
