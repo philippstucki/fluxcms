8bbc674430f64c965c3b94072c66c1428b73cfcb
chregu <chregu@99f7f646-bdd5-0310-89f9-ddcb10b10bf3>
2009-07-10 15:04:17 +0000

    remove requite
    git-svn-id: https://svn.liip.ch/repos/public/fluxcms/trunk@11777 99f7f646-bdd5-0310-89f9-ddcb10b10bf3

--- inc/Auth.php
+++ inc/Auth.php
@@ -478,7 +478,6 @@ class Auth {
     function &_factory($driver, $options = '')
     {
         $storage_class = 'Auth_Container_' . $driver;
-        include_once 'Auth/Container/' . $driver . '.php';
         $obj =& new $storage_class($options);
         return $obj;
     }
