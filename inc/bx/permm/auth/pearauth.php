b0e10ef6d8a70651a45fa408a24de101643d1076
chregu <chregu@99f7f646-bdd5-0310-89f9-ddcb10b10bf3>
2009-07-13 10:59:59 +0000

    CS WS
    git-svn-id: https://svn.liip.ch/repos/public/fluxcms/trunk@11788 99f7f646-bdd5-0310-89f9-ddcb10b10bf3

--- inc/bx/permm/auth/pearauth.php
+++ inc/bx/permm/auth/pearauth.php
@@ -9,7 +9,7 @@ Class bx_permm_auth_pearauth extends bx_permm_auth_common {
     public function __construct($options = array()) {
         parent::__construct();
         $options = $this->initOptions($options);
-        $this->MDB2Constructor($options,'MDB2',array("advancedsecurity"));
+        $this->MDB2Constructor($options, 'MDB2', array("advancedsecurity"));
 
     }
     // checks, if a password matches to the logged in user
