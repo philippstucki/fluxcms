8621c361cb1e63273bab7dcb46fc62bc2f0fe8d3
chregu <chregu@99f7f646-bdd5-0310-89f9-ddcb10b10bf3>
2009-08-31 14:57:46 +0000

    WS only
    git-svn-id: https://svn.liip.ch/repos/public/fluxcms/trunk@12268 99f7f646-bdd5-0310-89f9-ddcb10b10bf3

--- inc/bx/filters/patforms/form2mail.php
+++ inc/bx/filters/patforms/form2mail.php
@@ -3,27 +3,24 @@
 class bx_filters_patforms_form2mail extends bx_filters_patforms_formhandler {
 
     public function submitFields($params, $fields) {
-      
-        
+
         $options = array();
         $options['charset'] = !empty($params['charset']) ? $params['charset'] : "UTF-8";
-        
+
         $from = 'Do not reply to this address <nobody@example.org>';
-        if(!empty($params['emailFromField']) && !empty($fields[$params['emailFromField']])) {
+        if (!empty($params['emailFromField']) && !empty($fields[$params['emailFromField']])) {
             $from = $fields[$params['emailFromField']];
-        } else if(!empty($params['emailFrom'])) {
+        } else if (!empty($params['emailFrom'])) {
             $from = $params['emailFrom'];
         }
-        
-        if(strpos($from, "\n") !== FALSE or strpos($from, "\r") !== FALSE) { 
+
+        if (strpos($from, "\n") !== FALSE or strpos($from, "\r") !== FALSE) {
             return FALSE;
         }
-        $headers .= "From: $from\r\n";
 
         $emailSubject = !empty($params['subjectTemplateKey']) ? $this->getText($params['subjectTemplateKey']) : '';
 
-
-        if(strpos($emailSubject, "\n") !== FALSE or strpos($emailSubject, "\r") !== FALSE) { 
+        if (strpos($emailSubject, "\n") !== FALSE or strpos($emailSubject, "\r") !== FALSE) {
             return FALSE;
         }
 
@@ -31,16 +28,16 @@ class bx_filters_patforms_form2mail extends bx_filters_patforms_formhandler {
 
         // fields contain themselves as formatted string
         $fields['__allfields__'] = bx_helpers_string::formatTextFields($fields);
-        
+
         // replace textfields in subject and body
         $emailSubject = bx_helpers_string::replaceTextFields($emailSubject, $fields);
         $emailBody = bx_helpers_string::replaceTextFields($emailBody, $fields);
 
-        if(!empty($params['emailTo'])) {   
+        if (!empty($params['emailTo'])) {
             $n = bx_notificationmanager::getInstance("mail");
-            return $n->send($params['emailTo'],$emailSubject, $emailBody, $from, null,$options);
+            return $n->send($params['emailTo'], $emailSubject, $emailBody, $from, null, $options);
         }
         return FALSE;
     }
-    
+
 }
