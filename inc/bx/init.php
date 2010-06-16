d00540aac313495d81e879a43ebf27a29e838c18
liipbot <liipbot@99f7f646-bdd5-0310-89f9-ddcb10b10bf3>
2010-06-16 03:01:55 +0000

    automatic BuildDate Update
    git-svn-id: https://svn.liip.ch/repos/public/fluxcms/trunk@13655 99f7f646-bdd5-0310-89f9-ddcb10b10bf3

--- inc/bx/init.php
+++ inc/bx/init.php
@@ -1,7 +1,7 @@
 <?php
 
 define('BXCMS_VERSION', "1.6-dev");
-define('BXCMS_BUILD_DATE','10.6.10');
+define('BXCMS_BUILD_DATE','10.6.16');
 define('BXCMS_BUILD_HOUR','5.2');
 //define('BXCMS_REVISION',preg_replace('/\$Rev$'));
 define('BXCMS_REVISION', '$Rev$');
