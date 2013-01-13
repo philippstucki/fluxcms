2b354564dcce9a48c91cef8f7af637bd43bbc9bb
Philipp Stucki <philipp@qwert.ch>
2013-01-13 11:47:08 +0100

    WS only

--- webinc/plugins/dbforms2/transport.js
+++ webinc/plugins/dbforms2/transport.js
@@ -5,9 +5,9 @@ function dbforms2_transport() {
     this.dataSaved = false;
     this.onLoadCallback = null;
     this.onSaveCallback = null;
-    
+
     this.loadXML = function(dataURI) {
-        
+
         dbforms2_log.log('loading ' + dataURI + '...')
         this.dataLoaded = false;
 
@@ -16,22 +16,22 @@ function dbforms2_transport() {
             context: this,
             success: function(d, s, xhr) {
                 this.dataLoaded = true;
-                
+
                 this.data = xhr.responseXML;
-    
+
                 // call document ready callback when the document has been loaded
                 if (this.onLoadCallback != null && typeof this.onLoadCallback == "function")
                     this.onLoadCallback();
             }
         });
-        
+
     }
-    
+
     this.saveXML = function(dataURI, xml) {
-        
+
         dbforms2_log.log('saving ' + dataURI + '...')
         this.dataSaved = false;
-        
+
         jQuery.ajax({
             type: 'POST',
             url: dataURI,
@@ -44,29 +44,29 @@ function dbforms2_transport() {
 
                 response = new dbforms2_response();
                 response.setXML(xhr.responseXML);
-                
+
                 // call document ready callback when the document has been saved
                 if (this.onSaveCallback != null && typeof this.onSaveCallback == "function")
                     this.onSaveCallback(response);
             }
         });
-        
+
     }
-    
+
     this.saveXMLSync = function(dataURI, xml) {
         this.data = new XMLHttpRequest();
         this.data.open('POST', dataURI, false);
         response = new dbforms2_response();
-        
+
         try {
             this.data.send(xml);
             response.setXML(this.data.responseXML);
         } catch (e) {
             response.responseText = 'Unable to establish a connection to the server.';
         }
-        
+
         return response;
-        
+
     }
 
 }
@@ -78,18 +78,18 @@ function dbforms2_response() {
     this.responseText = '';
     this.savedID = 0;
     this.responseData = null;
-    
+
     this.setXML = function(xml) {
         this.xml = xml;
         this.parseXML();
     }
-    
+
     this.parseXML = function() {
         // parse response code and message
         this.responseCode = this.xml.documentElement.getAttribute('code');
         this.savedID = this.xml.documentElement.getAttribute('id');
         this.responseText = this.xml.documentElement.getElementsByTagName('text')[0].childNodes[0].data;
-        
+
         // check if the server has returned new data
         var dataNS = this.xml.documentElement.getElementsByTagName('data');
         if(dataNS[0]) {
@@ -100,24 +100,24 @@ function dbforms2_response() {
             } else {
                 xml.documentElement.appendChild(dataNS[0]);
             }
-            
+
             this.responseData = xml;
         }
     }
-    
+
     this.isError = function() {
-        if(this.responseCode != 0) 
+        if(this.responseCode != 0)
             return true;
 
         return false;
     }
-    
+
     this.getResponseText = function() {
-        return this.responseText; 
+        return this.responseText;
     }
-    
+
     this.getResponseCode = function() {
-        return this.responseCode; 
+        return this.responseCode;
     }
-    
+
 }
